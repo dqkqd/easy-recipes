5bf75be81235dd09db10168abdbef8699431d122
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-15 03:41:32 +0900

    Change es to erecipes.

--- backend/erecipes/__init__.py
+++ backend/erecipes/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
-from es.config import BaseConfig, Config
+from erecipes.config import BaseConfig, Config
 
 
 def create_app(config: type[BaseConfig] = Config) -> Flask:
     app = Flask(__name__)
     app.config.from_object(config)
 
-    from es.models import db, migrate
+    from erecipes.models import db, migrate
 
     db.init_app(app)
     migrate.init_app(app, db)
