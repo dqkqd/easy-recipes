16a502482a73081614c3b66b020b2635c52457b6
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-16 03:34:21 +0900

    Update deployment for database and backend app.

--- backend/app/config.py
+++ backend/app/config.py
@@ -23,13 +23,13 @@ class BaseConfig:
 
 
 class Config(BaseConfig):
-    DB_USER = os.environ.get("DB_USER")
-    DB_PASSWD = os.environ.get("DB_PASSWD")
-    DB_HOST = os.environ.get("DB_HOST")
-    DB_PORT = os.environ.get("DB_PORT")
-    DB_NAME = os.environ.get("DB_NAME")
+    POSTGRES_USER = os.environ.get("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    POSTGRES_HOST = os.environ.get("POSTGRES_HOST")
+    POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
+    POSTGRES_DB = os.environ.get("POSTGRES_DB")
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 class TestingConfig(BaseConfig):
