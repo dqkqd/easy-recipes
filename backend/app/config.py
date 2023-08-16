979e675603e24d974ed0338febb36324efaf74f6
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-17 03:32:31 +0900

    Run app inside docker from now. No need dot_env.

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,25 +1,20 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 BASE_DIR = Path(__file__).parent
 
 
 class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    DEFAULT_INGREDIENT_IMAGE_NAME = "default-ingredient-image.jpg"
+    DEFAULT_RECIPE_IMAGE_NAME = "default-recipe-image.jpg"
+
+    # TODO(dqk): keeping them here for testing, remove after setting up docker
     DEFAULT_INGREDIENT_IMAGE = (
         BASE_DIR.parent / "static" / "images" / "default-ingredient-image.jpg"
     )
-    if not Path(DEFAULT_INGREDIENT_IMAGE).is_file():
-        raise FileNotFoundError(DEFAULT_INGREDIENT_IMAGE)
-
     DEFAULT_RECIPE_IMAGE = BASE_DIR.parent / "static" / "images" / "default-recipe-image.jpg"
-    if not Path(DEFAULT_RECIPE_IMAGE).is_file():
-        raise FileNotFoundError(DEFAULT_RECIPE_IMAGE)
 
 
 class Config(BaseConfig):
@@ -31,9 +26,23 @@ class Config(BaseConfig):
 
     SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
+    FILE_SERVER_HOST = os.environ.get("FILE_SERVER_HOST")
+    FILE_SERVER_PORT = os.environ.get("FILE_SERVER_PORT")
+    FILE_SERVER_IMAGE_DIRECTORY = os.environ.get("FILE_SERVER_IMAGE_DIRECTORY")
+
+    FILE_SERVER_URI = f"http://{FILE_SERVER_HOST}:{FILE_SERVER_PORT}/{FILE_SERVER_IMAGE_DIRECTORY}"
+
+    DEFAULT_INGREDIENT_IMAGE_URI = f"{FILE_SERVER_URI}/{BaseConfig.DEFAULT_INGREDIENT_IMAGE}"
+    DEFAULT_RECIPE_IMAGE_URI = f"{FILE_SERVER_URI}/{BaseConfig.DEFAULT_RECIPE_IMAGE}"
+
 
 class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     TEST_DB_PATH = os.environ.get("TEST_DB_PATH", "test.db")
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{TEST_DB_PATH}"
+
+    FILE_SERVER_URI = (BASE_DIR.parent / "static" / "images").as_uri()
+
+    DEFAULT_INGREDIENT_IMAGE_URI = f"{FILE_SERVER_URI}/{BaseConfig.DEFAULT_INGREDIENT_IMAGE}"
+    DEFAULT_RECIPE_IMAGE_URI = f"{FILE_SERVER_URI}/{BaseConfig.DEFAULT_RECIPE_IMAGE}"
