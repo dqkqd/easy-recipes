fbc8a990281cc8417fb000ed8596df3fd0cf42ca
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-16 17:32:31 +0900

    Remove unneeded Base class implementation.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,16 +4,7 @@ from flask_sqlalchemy.model import Model
 
 
 class Base(Model):
-    def insert(self) -> None:
-        db.session.add(self)
-        db.session.commit()
-
-    def delete(self) -> None:
-        db.session.delete(self)
-        db.session.commit()
-
-    def update(self) -> None:
-        db.session.commit()
+    ...
 
 
 db = SQLAlchemy(model_class=Base)
