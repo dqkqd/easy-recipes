3b1e41ce9f2eaa3145edad8d89f28f1d67b6148c
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-16 18:12:26 +0900

    Add todo rule.

--- backend/app/routes/recipe.py
+++ backend/app/routes/recipe.py
@@ -8,7 +8,7 @@ api = Blueprint("recipes", __name__, url_prefix="/recipes")
 
 @api.route("/")
 def get_recipes() -> Response:
-    # @TODO(dqk): filter model's fields
+    # TODO(dqk): filter model's fields
     recipes = Recipe.query.all()
     print(recipes)
     return jsonify({"recipes": recipes})
