389f4540d1a6f6a777e7b4ab1789e54c6c44a8c9
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-18 07:48:59 +0900

    Add more tests.

--- fileserver/tests/test_api.py
+++ fileserver/tests/test_api.py
@@ -1,4 +1,5 @@
 import json
+import os
 from pathlib import Path
 
 from flask.testing import FlaskClient
@@ -84,5 +85,54 @@ def test_200_get_image(tmp_path: Path, client: FlaskClient) -> None:
     encrypted_filename = data["filename"]
     response = client.get(f"/images/{encrypted_filename}")
     assert response.status_code == 200
-
     assert response.data == file.read_bytes()
+
+
+def test_200_get_image_does_not_take_old_image(tmp_path: Path, client: FlaskClient) -> None:
+    # make sure our image folder is empty
+    image_folder: Path = client.application.config["IMAGE_FOLDER"]
+    assert not os.listdir(image_folder)
+
+    file = tmp_path / "file.txt"
+    with file.open("wb") as f:
+        f.write(bytearray(1000))
+
+    files = {"file": file.open("rb")}
+    response = client.post("/images/", data=files)
+    assert response.status_code == 200
+    file_bytes = file.read_bytes()
+    file.rename("new-file.txt")
+
+    data = json.loads(response.data)
+    encrypted_filename = data["filename"]
+    response = client.get(f"/images/{encrypted_filename}")
+    assert response.status_code == 200
+    assert response.data == file_bytes
+
+
+def test_404_get_non_existed_image(tmp_path: Path, client: FlaskClient) -> None:
+    # make sure our image folder is empty
+    image_folder: Path = client.application.config["IMAGE_FOLDER"]
+    assert not os.listdir(image_folder)
+
+    file = tmp_path / "file.txt"
+    with file.open("wb") as f:
+        f.write(bytearray(1000))
+
+    files = {"file": file.open("rb")}
+    response = client.post("/images/", data=files)
+    assert response.status_code == 200
+
+    for file in os.listdir(image_folder):
+        (image_folder / file).unlink()
+
+    data = json.loads(response.data)
+    encrypted_filename = data["filename"]
+    response = client.get(f"/images/{encrypted_filename}")
+    assert response.status_code == 404
+
+
+def test_404_get_image_no_filename_encrypted(client: FlaskClient) -> None:
+    handler = UniqueFilenameHandler()
+    response = client.get(f"/images/{handler.encrypted_filename}")
+    assert response.status_code == 404
