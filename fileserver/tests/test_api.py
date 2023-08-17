5f9ea01f4361c0c8203fe8b7b8e75cf6cf46b3ed
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-17 23:03:26 +0900

    Saved file is not the same file but has the same content.

--- fileserver/tests/test_api.py
+++ fileserver/tests/test_api.py
@@ -1,9 +1,13 @@
 import json
+from typing import TYPE_CHECKING
 
 from flask.testing import FlaskClient
 
 from app import constants
 from app.filename_handler import UniqueFilenameHandler
+
+if TYPE_CHECKING:
+    from pathlib import Path
 
 
 def test_200_get_default_ingredient(client: FlaskClient) -> None:
@@ -19,7 +23,8 @@ def test_200_get_default_recipe(client: FlaskClient) -> None:
 
 
 def test_200_upload_image(client: FlaskClient) -> None:
-    files = {"file": constants.DEFAULT_RECIPE_IMAGE.open("rb")}
+    default_recipe_image = constants.DEFAULT_RECIPE_IMAGE
+    files = {"file": default_recipe_image.open("rb")}
     response = client.post("/images/", data=files)
     assert response.status_code == 200
 
@@ -27,6 +32,8 @@ def test_200_upload_image(client: FlaskClient) -> None:
     encrypted_filename = data["filename"]
     handler = UniqueFilenameHandler.from_encrypted_filename(encrypted_filename)
 
-    image_folder = client.application.config["IMAGE_FOLDER"]
+    image_folder: Path = client.application.config["IMAGE_FOLDER"]
     saved_file = image_folder / handler.filename
     assert saved_file.is_file()
+    assert not saved_file.samefile(default_recipe_image)
+    assert saved_file.read_bytes() == default_recipe_image.read_bytes()
