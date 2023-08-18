63ce7cdb3e47afd006c3a91746a087d03826e105
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-19 02:03:30 +0900

    Remove tests from file.

--- backend/tests/integrations/test_file_server_handler.py
+++ backend/tests/integrations/test_file_server_handler.py
@@ -1,5 +1,4 @@
 import io
-from pathlib import Path
 
 import pytest
 import requests
@@ -20,22 +19,10 @@ def test_upload_image_from_bytes() -> None:
     image_bytes = io.BytesIO()
     img.save(image_bytes, format="PNG")
 
-    identifier = file_server.upload_image_from_bytes(image_bytes)
+    identifier = file_server.add(image_bytes)
     requested_image_bytes = file_server.get(identifier)
 
     assert image_bytes.getvalue() == requested_image_bytes.getvalue()
-
-
-@pytest.mark.usefixtures("app")
-def test_upload_image_from_file(tmp_path: Path) -> None:
-    file = tmp_path / "image.png"
-    img = Image.new("RGB", (256, 256))
-    img.save(file, format="PNG")
-
-    identifier = file_server.upload_image_from_file(file)
-    requested_image_bytes = file_server.get(identifier)
-
-    assert file.read_bytes() == requested_image_bytes.getvalue()
 
 
 @pytest.mark.skip()
@@ -44,10 +31,10 @@ def test_upload_image_from_url() -> None:
     img = Image.new("RGB", (256, 256))
     image_bytes = io.BytesIO()
     img.save(image_bytes, format="PNG")
-    identifier = file_server.upload_image_from_bytes(image_bytes)
+    identifier = file_server._add_bytes(image_bytes)
     image_url = file_server.uri(identifier)
 
-    new_identifier = file_server.upload_image_from_url(image_url)
+    new_identifier = file_server._add_url(image_url)
     requested_image_bytes = file_server.get(identifier)
 
     assert identifier != new_identifier
