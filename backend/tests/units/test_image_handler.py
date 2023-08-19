3ca629dd507325782abd94371e3134cf94286bdb
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-19 18:24:23 +0900

    Unskip test.

--- backend/tests/units/test_image_handler.py
+++ backend/tests/units/test_image_handler.py
@@ -28,23 +28,22 @@ def test_add_from_file(tmp_path: Path) -> None:
         assert file.read_bytes() == image_on_server.byte_data.getvalue()
 
 
-@pytest.mark.skip()
 def test_add_from_url() -> None:
     img = random_image(config.MAX_IMAGE_SIZE, config.MAX_IMAGE_SIZE)
     image_bytes = io.BytesIO()
     img.save(image_bytes, format="PNG")
 
     with ImageOnServer.from_bytes(image_bytes) as image_on_server:
-        image_url = image_on_server.uri
+        identifier = image_on_server.identifier
+        image_url = image_on_server.as_uri()
         assert image_bytes.getvalue() == image_on_server.byte_data.getvalue()
 
-    image_identifier = ImageOnServer.from_bytes(image_bytes).upload(fs)
+    with ImageOnServer.from_url(image_url) as image_on_server:
+        new_identifier = image_on_server.identifier
+        new_image_bytes = image_on_server.byte_data
 
-    new_image_identifier = ImageOnServer.from_url(image_url).upload(fs)
-    responsed_bytes = fs.get(new_image_identifier)
-
-    assert image_identifier != new_image_identifier
-    assert image_bytes.getvalue() == responsed_bytes.getvalue()
+    assert identifier != new_identifier
+    assert image_bytes.getvalue() == new_image_bytes.getvalue()
 
 
 def test_add_from_bytes_clean_up_after_exception() -> None:
