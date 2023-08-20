a19cdd303f1ccb204f1c37f50a48f2450980ea12
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-21 01:31:08 +0900

    Copy tests and implementation from ingredients.

--- backend/app/routes/recipe.py
+++ backend/app/routes/recipe.py
@@ -1,13 +1,80 @@
-from flask import Blueprint, jsonify
-from werkzeug import Response
+from __future__ import annotations
 
-from app.models.recipe import Recipe
+from typing import TYPE_CHECKING
+
+from flask import Blueprint, abort, jsonify, request
+
+from app.crud import crud_recipe
+from app.errors import to_handleable_error
+from app.file_server.image import ImageOnServer
+from app.schemas.recipe import RecipeCreate, RecipeInDB
+
+if TYPE_CHECKING:
+    from werkzeug import Response
 
 api = Blueprint("recipes", __name__, url_prefix="/recipes")
 
 
-@api.route("/")
+@api.route("/all")
+@to_handleable_error
 def get_recipes() -> Response:
-    # TODO(dqk): filter model's fields
-    recipes = Recipe.query.all()
-    return jsonify({"recipes": recipes})
+    recipes = crud_recipe.get_all()
+    return jsonify(
+        {
+            "total": len(recipes),
+            "recipes": [
+                recipe.to_schema(RecipeInDB).to_public().model_dump(mode="json")
+                for recipe in recipes
+            ],
+        },
+    )
+
+
+@api.route("/")
+@to_handleable_error
+def get_paginations() -> Response:
+    paginaged_recipes = crud_recipe.get_pagination()
+
+    if paginaged_recipes.page != 1 and paginaged_recipes.page > paginaged_recipes.pages:
+        abort(404)
+
+    return jsonify(
+        {
+            "page": paginaged_recipes.page,
+            "recipes": [
+                recipe.to_schema(RecipeInDB).to_public().model_dump(mode="json")
+                for recipe in paginaged_recipes
+            ],
+            "total": paginaged_recipes.total,
+        },
+    )
+
+
+@api.route("/<int:id>")
+@to_handleable_error
+def get_recipe(id: int) -> Response:  # noqa: A002
+    return jsonify(
+        crud_recipe.get(id).to_schema(RecipeInDB).to_public().model_dump(mode="json"),
+    )
+
+
+@api.route("/", methods=["POST"])
+@to_handleable_error
+def create_recipe() -> Response:
+    body = request.get_json()
+
+    recipe_create = RecipeCreate(**body)
+
+    if recipe_create.image_uri is not None:
+        with ImageOnServer.from_source(recipe_create.image_uri) as image_on_server:
+            recipe_create.image_uri = image_on_server.uri
+
+    recipe = crud_recipe.add(recipe_create)
+    return jsonify({"id": recipe.id})
+
+
+@api.route("/<int:id>", methods=["DELETE"])
+@to_handleable_error
+def delete_recipe(id: int) -> Response:  # noqa: A002
+    crud_recipe.delete(id)
+    return jsonify({"id": id})
--- backend/tests/units/test_recipes.py
+++ backend/tests/units/test_recipes.py
@@ -1,29 +1,381 @@
+from __future__ import annotations
+
 import json
+from typing import TYPE_CHECKING
 
 import pytest
-from flask.testing import FlaskClient
+from pydantic_core import Url
+
+from app import config
+from app.crud import crud_recipe
+from app.schemas.recipe import RecipePublic
+from tests import mock_data
+from tests.utils import compare_image_data_from_uri
+
+if TYPE_CHECKING:
+    from flask.testing import FlaskClient
 
 
-def test_get_recipes_no_recipes(client: FlaskClient) -> None:
-    response = client.get("/recipes/")
-    assert response.status_code == 200
+@pytest.mark.usefixtures("app_context")
+def test_200_create_basic(client: FlaskClient) -> None:
+    recipe_create = mock_data.MockRecipe.random_valid_recipe()
+    response = client.post(
+        "/recipes/",
+        json=recipe_create.model_dump(mode="json"),
+    )
 
     data = json.loads(response.data)
-    assert len(data["recipes"]) == 0
+    assert data == {"id": 1}
+    assert response.status_code == 200
+
+    recipe = crud_recipe.get(id=1)
+
+    assert recipe.id == 1
+    assert recipe.name == recipe_create.name
+
+    # same image but different url
+    assert Url(recipe.image_uri) != recipe_create.image_uri
+    assert compare_image_data_from_uri(
+        Url(recipe.image_uri),
+        recipe_create.image_uri,
+    )
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        None,
+        "",
+        " ",
+    ],
+)
+def test_422_create_invalid_name(client: FlaskClient, name: str | None) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data["name"] = name
+    response = client.post("/recipes/", json=recipe_data)
+
+    data = json.loads(response.data)
+    assert response.status_code == 422
+    assert data == {"code": 422, "message": "Invalid name."}
+
+
+def test_422_create_no_name_provided(client: FlaskClient) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data.pop("name")
+    response = client.post("/recipes/", json=recipe_data)
+
+    data = json.loads(response.data)
+    assert response.status_code == 422
+    assert data == {"code": 422, "message": "Invalid name."}
+
+
+@pytest.mark.usefixtures("app_context")
+def test_200_create_name_stripped(client: FlaskClient) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data["name"] = "   eggs  "
+
+    response = client.post(
+        "/recipes/",
+        json=recipe_data,
+    )
+
+    assert response.status_code == 200
+
+    recipe = crud_recipe.get(id=1)
+    assert recipe.name == "eggs"
+
+
+@pytest.mark.usefixtures("app_context")
+def test_200_create_empty_image_uri(client: FlaskClient) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data["image_uri"] = None
+    response = client.post("/recipes/", json=recipe_data)
+
+    data = json.loads(response.data)
+    assert data == {"id": 1}
+    assert response.status_code == 200
+
+    recipe = crud_recipe.get(id=1)
+    assert recipe.image_uri is None
+
+
+@pytest.mark.usefixtures("app_context")
+def test_200_create_no_image_uri_provided(client: FlaskClient) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data.pop("image_uri")
+    response = client.post("/recipes/", json=recipe_data)
+
+    data = json.loads(response.data)
+    assert data == {"id": 1}
+    assert response.status_code == 200
+
+    recipe = crud_recipe.get(id=1)
+    assert recipe.image_uri is None
+
+
+@pytest.mark.usefixtures("app_context")
+def test_422_create_invalid_uri(client: FlaskClient) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data["image_uri"] = "invalid"
+    response = client.post("/recipes/", json=recipe_data)
+
+    data = json.loads(response.data)
+    assert response.status_code == 422
+    assert data == {"code": 422, "message": "Invalid image_uri."}
+
+
+@pytest.mark.usefixtures("app_context")
+def test_415_create_valid_uri_but_invalid_image(client: FlaskClient) -> None:
+    recipe_data = mock_data.MockRecipe.random_valid_recipe_data()
+    recipe_data["image_uri"] = "https://example.com"
+    response = client.post("/recipes/", json=recipe_data)
+
+    data = json.loads(response.data)
+    assert response.status_code == 415
+    assert data == {"code": 415, "message": "Invalid image."}
+
+
+@pytest.mark.skip("Update after implementing recipes")
+def test_200_create_recipe_with_added_recipes() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.skip("Update after implementing recipes")
+def test_422_create_recipe_with_invalid_recipes() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.skip("Update after implementing authorization")
+def test_401_create_unauthorize(client: FlaskClient) -> None:  # noqa: ARG001
+    raise NotImplementedError
+
+
+@pytest.mark.skip("Update after implementing authorization")
+def test_403_create_no_permission(client: FlaskClient) -> None:  # noqa: ARG001
+    raise NotImplementedError
+
+
+def test_200_get_recipe_basic(client: FlaskClient) -> None:
+    recipe_create = mock_data.MockRecipe.random_valid_recipe()
+    response = client.post(
+        "/recipes/",
+        json=recipe_create.model_dump(mode="json"),
+    )
+
+    response = client.get("/recipes/1")
+    data = json.loads(response.data)
+    recipe_public = RecipePublic(**data)
+
+    assert response.status_code == 200
+    assert recipe_create.model_dump(
+        exclude={"image_uri"},
+    ) == recipe_public.model_dump(exclude={"id", "image_uri"})
+
+    assert recipe_create.image_uri != recipe_public.image_uri
+    assert compare_image_data_from_uri(
+        recipe_create.image_uri,
+        recipe_public.image_uri,
+    )
+
+
+def test_200_get_recipe_after_many_create(client: FlaskClient) -> None:
+    num_datas = 5
+
+    for _ in range(num_datas):
+        client.post(
+            "/recipes/",
+            json=mock_data.MockRecipe.random_valid_recipe_data(),
+        )
+
+    for recipe_id in range(1, num_datas + 1):
+        response = client.get(f"/recipes/{recipe_id}")
+        data = json.loads(response.data)
+
+        assert response.status_code == 200
+        assert data["id"] == recipe_id
 
 
 @pytest.mark.skip()
-def test_get_recipes_has_recipes(client: FlaskClient) -> None:
-    msg = "Implement a fixture that automatically add mock data"
-    raise NotImplementedError(msg)
-    msg = "Get recipes per pages"
-    raise NotImplementedError(msg)
+def test_200_get_recipe_with_added_recipes() -> None:
+    raise NotImplementedError
 
-    response = client.get("/recipes/")
+
+@pytest.mark.skip()
+def test_200_get_recipe_should_give_proper_image() -> None:
+    raise NotImplementedError
+
+
+@pytest.mark.skip()
+def test_200_get_recipe_unwated_items_are_not_exposed() -> None:
+    raise NotImplementedError
+
+
+def test_404_get_invalid_recipe(client: FlaskClient) -> None:
+    response = client.get("/recipes/1")
+    data = json.loads(response.data)
+    assert data == {"code": 404, "message": "Resources not found."}
+    assert response.status_code == 404
+
+
+def test_200_delete_basic(client: FlaskClient) -> None:
+    client.post(
+        "/recipes/",
+        json=mock_data.MockRecipe.random_valid_recipe_data(),
+    )
+    response = client.get("/recipes/1")
+    assert response.status_code == 200
+
+    response = client.delete("/recipes/1")
+    assert response.status_code == 200
+
+
+def test_404_delete_non_existed(client: FlaskClient) -> None:
+    response = client.delete("/recipes/1")
+    assert response.status_code == 404
+
+    data = json.loads(response.data)
+    assert data == {"code": 404, "message": "Resources not found."}
+
+
+def test_404_delete_twice(client: FlaskClient) -> None:
+    client.post(
+        "/recipes/",
+        json=mock_data.MockRecipe.random_valid_recipe_data(),
+    )
+    response = client.delete("/recipes/1")
+    assert response.status_code == 200
+
+    response = client.delete("/recipes/1")
+    assert response.status_code == 404
+
+    data = json.loads(response.data)
+    assert data == {"code": 404, "message": "Resources not found."}
+
+
+def test_200_get_all(client: FlaskClient) -> None:
+    num_datas = 15
+
+    inserted_recipes_data = []
+    for _ in range(num_datas):
+        data = mock_data.MockRecipe.random_valid_recipe_data()
+        client.post("/recipes/", json=data)
+        inserted_recipes_data.append(data)
+
+    response = client.get("/recipes/all")
     assert response.status_code == 200
 
     data = json.loads(response.data)
-    assert len(data) > 0
+    assert data["total"] == num_datas
 
-    msg = "Make sure responsed data is equal ..."
-    raise NotImplementedError(msg)
+    for response_data, inserted_data in zip(
+        data["recipes"],
+        inserted_recipes_data,
+    ):
+        response_data.pop("image_uri")
+        response_data.pop("id")
+
+        inserted_data.pop("image_uri")
+
+        assert response_data == inserted_data
+
+
+def test_200_get_all_no_data(client: FlaskClient) -> None:
+    response = client.get("/recipes/all")
+    assert response.status_code == 200
+
+    data = json.loads(response.data)
+    assert data == {"total": 0, "recipes": []}
+
+
+def test_200_get_all_after_deletion(client: FlaskClient) -> None:
+    num_datas = 15
+    num_delete_datas = num_datas // 2
+
+    inserted_recipes_data = []
+    for _ in range(num_datas):
+        data = mock_data.MockRecipe.random_valid_recipe_data()
+        client.post("/recipes/", json=data)
+        inserted_recipes_data.append(data)
+
+    response = client.get("/recipes/all")
+    assert response.status_code == 200
+
+    for recipe_id in range(1, num_delete_datas + 1):
+        response = client.delete(f"/recipes/{recipe_id}")
+        assert response.status_code == 200
+
+    response = client.get("/recipes/all")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data["total"] == num_datas - num_delete_datas
+
+
+def test_200_get_pagination_basic(client: FlaskClient) -> None:
+    total_pages = 2
+    last_page_items = config.PAGINATION_SIZE // 2
+    num_datas = total_pages * config.PAGINATION_SIZE + last_page_items
+
+    for _ in range(num_datas):
+        data = mock_data.MockRecipe.random_valid_recipe_data()
+        client.post("/recipes/", json=data)
+
+    for page in range(1, total_pages + 2):
+        response = client.get(f"/recipes/?page={page}")
+        assert response.status_code == 200
+        data = json.loads(response.data)
+        assert data["total"] == num_datas
+
+        assert data["page"] == page
+        if page != total_pages + 1:
+            assert len(data["recipes"]) == config.PAGINATION_SIZE
+        else:
+            assert len(data["recipes"]) == last_page_items
+
+
+def test_200_get_pagination_no_param(client: FlaskClient) -> None:
+    data = mock_data.MockRecipe.random_valid_recipe_data()
+    client.post("/recipes/", json=data)
+
+    response = client.get("/recipes/")
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data["total"] == 1
+    assert data["page"] == 1
+    assert len(data["recipes"]) == 1
+
+
+def test_404_get_page_does_not_exist(client: FlaskClient) -> None:
+    data = mock_data.MockRecipe.random_valid_recipe_data()
+    client.post("/recipes/", json=data)
+
+    response = client.get("/recipes/100")
+    data = json.loads(response.data)
+    assert response.status_code == 404
+    assert data == {"code": 404, "message": "Resources not found."}
+
+
+def test_404_get_page_no_items(client: FlaskClient) -> None:
+    response = client.get("/recipes/")
+    data = json.loads(response.data)
+    assert data == {"page": 1, "recipes": [], "total": 0}
+    assert response.status_code == 200
+
+    response = client.get("/recipes/?page=1")
+    data = json.loads(response.data)
+    assert response.status_code == 200
+    assert data == {"page": 1, "recipes": [], "total": 0}
+
+
+def test_200_404_pagination_render_enough(client: FlaskClient) -> None:
+    for _ in range(config.PAGINATION_SIZE * 2):
+        data = mock_data.MockRecipe.random_valid_recipe_data()
+        client.post("/recipes/", json=data)
+
+    response = client.get("/recipes/?page=1")
+    assert response.status_code == 200
+
+    response = client.get("/recipes/?page=2")
+    assert response.status_code == 200
+
+    response = client.get("/recipes/?page=3")
+    assert response.status_code == 404
