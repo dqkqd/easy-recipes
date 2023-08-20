44cc50c832dee909f6a41207e93d9104bc933007
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-20 18:13:22 +0900

    Add generic type.

--- backend/app/crud/base.py
+++ backend/app/crud/base.py
@@ -1,19 +1,35 @@
 from contextlib import contextmanager
-from typing import Iterator, Self
+from typing import Generic, Iterator, Self, TypeVar
 
 from flask import current_app, has_app_context
 from flask_sqlalchemy import SQLAlchemy
-from flask_sqlalchemy.model import Model
+
+from app import database
+from app.schemas.base import BaseSchema
+
+ModelType = TypeVar("ModelType", bound=database.Base)
+InDBSChemaType = TypeVar("InDBSChemaType", bound=BaseSchema)
+PublicSchemaType = TypeVar("PublicSchemaType", bound=BaseSchema)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseSchema)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseSchema)
 
 
-class CRUDBase:
+class CRUDBase(
+    Generic[
+        ModelType,
+        CreateSchemaType,
+        UpdateSchemaType,
+        InDBSChemaType,
+        PublicSchemaType,
+    ],
+):
     def __init__(self, db: SQLAlchemy) -> None:
         self.db = db
 
-    def add(self, instance: Model) -> None:
+    def add(self, instance: ModelType) -> None:
         self.db.session.add(instance)
 
-    def delete(self, instance: Model) -> None:
+    def delete(self, instance: ModelType) -> None:
         self.db.session.delete(instance)
 
     def commit(self) -> None:
--- backend/app/crud/ingredient.py
+++ backend/app/crud/ingredient.py
@@ -1,9 +1,22 @@
 from app.crud.base import CRUDBase
 from app.models.ingredient import Ingredient
-from app.schemas.ingredient import IngredientCreate, IngredientInDB
+from app.schemas.ingredient import (
+    IngredientCreate,
+    IngredientInDB,
+    IngredientPublic,
+    IngredientUpdate,
+)
 
 
-class CRUDIngredient(CRUDBase):
+class CRUDIngredient(
+    CRUDBase[
+        Ingredient,
+        IngredientCreate,
+        IngredientUpdate,
+        IngredientInDB,
+        IngredientPublic,
+    ],
+):
     def create(
         self,
         ingredient_create: IngredientCreate,
