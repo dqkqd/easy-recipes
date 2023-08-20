9f3e2bd59a0a71bc90b1b7cbb6d326bac9ee3484
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-21 01:30:23 +0900

    Implement crud for recipe.

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,4 +1,7 @@
 from app.crud.ingredient import CRUDIngredient
+from app.crud.recipe import CRUDRecipe
 from app.models.ingredient import Ingredient
+from app.models.recipe import Recipe
 
 crud_ingredient = CRUDIngredient(Ingredient)
+crud_recipe = CRUDRecipe(Recipe)
---
+++ backend/app/crud/recipe.py
@@ -0,0 +1,13 @@
+from app.crud.base import CRUDBase
+from app.models.recipe import Recipe
+from app.schemas.recipe import RecipeCreate, RecipeUpdate
+
+
+class CRUDRecipe(
+    CRUDBase[
+        Recipe,
+        RecipeCreate,
+        RecipeUpdate,
+    ],
+):
+    ...
