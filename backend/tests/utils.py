687626650d0bcdf5dcf9df1f101e7c9158171501
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-20 15:18:46 +0900

    mypy.

--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,7 +1,14 @@
-from pydantic_core import Url
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from app.file_server import fs
 
+if TYPE_CHECKING:
+    from pydantic_core import Url
 
-def compare_image_data_from_uri(uri1: Url, uri2: Url) -> bool:
+
+def compare_image_data_from_uri(uri1: Url | None, uri2: Url | None) -> bool:
+    if uri1 is None or uri2 is None:
+        return uri1 is None and uri2 is None
     return fs.get_from_uri(uri1).getvalue() == fs.get_from_uri(uri2).getvalue()
