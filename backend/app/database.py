0cd2024d91afa112f63208a5ebf65c8334457019
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-20 20:24:33 +0900

    Allow db to rollback on failure.

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,3 +1,6 @@
+from contextlib import contextmanager
+from typing import Iterator
+
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_sqlalchemy.model import Model
@@ -9,3 +12,13 @@ class Base(Model):
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
+
+
+@contextmanager
+def safe_db() -> Iterator[SQLAlchemy]:
+    try:
+        yield db
+    except Exception:
+        db.session.rollback()
+        db.session.close()
+        raise
