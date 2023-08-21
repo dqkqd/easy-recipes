8d1978b45c4f643520dc82e2e310bfbda8bce656
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-21 23:17:48 +0900

    Add more 401 tests.

--- backend/tests/units/test_auth.py
+++ backend/tests/units/test_auth.py
@@ -60,56 +60,6 @@ def test_200_delete_recipe(client: FlaskClient) -> None:
     assert response.status_code == 200
 
 
-def test_401_create_and_delete_missing_authorization_ingredient(
-    client: FlaskClient,
-) -> None:
-    response = client.post(
-        "/ingredients/",
-        json=MockIngredient.random_valid_ingredient_data(),
-    )
-    assert response.status_code == 401
-    data = json.loads(response.data)
-    assert data == {"code": 401, "message": "Unauthorized."}
-
-    client.post(
-        "/ingredients/",
-        headers=MockAuth.authorization_header(auth.CREATE_INGREDIENT_PERMISSION),
-        json=MockIngredient.random_valid_ingredient_data(),
-    )
-    response = client.delete(
-        "/ingredients/1",
-        json=MockIngredient.random_valid_ingredient_data(),
-    )
-    assert response.status_code == 401
-    data = json.loads(response.data)
-    assert data == {"code": 401, "message": "Unauthorized."}
-
-
-def test_401_create_and_delete_missing_authorization_recipe(
-    client: FlaskClient,
-) -> None:
-    response = client.post(
-        "/recipes/",
-        json=MockRecipe.random_valid_recipe_data(),
-    )
-    assert response.status_code == 401
-    data = json.loads(response.data)
-    assert data == {"code": 401, "message": "Unauthorized."}
-
-    client.post(
-        "/recipes/",
-        headers=MockAuth.authorization_header(auth.CREATE_RECIPE_PERMISSION),
-        json=MockRecipe.random_valid_recipe_data(),
-    )
-    response = client.delete(
-        "/recipes/1",
-        json=MockRecipe.random_valid_recipe_data(),
-    )
-    assert response.status_code == 401
-    data = json.loads(response.data)
-    assert data == {"code": 401, "message": "Unauthorized."}
-
-
 @pytest.mark.parametrize(
     "headers",
     [
@@ -131,8 +81,77 @@ def test_401_create_ingredient_authorization_invalid(
     data = json.loads(response.data)
     assert data == {"code": 401, "message": "Unauthorized."}
 
+
+@pytest.mark.parametrize(
+    "headers",
+    [
+        {},
+        {"Authorization": ""},
+        {"Authorization": "Bearer"},
+    ],
+)
+def test_401_delete_ingredient_authorization_invalid(
+    headers: dict[str, str],
+    client: FlaskClient,
+) -> None:
+    response = client.post(
+        "/ingredients/",
+        headers=MockAuth.authorization_header(auth.CREATE_INGREDIENT_PERMISSION),
+        json=MockIngredient.random_valid_ingredient_data(),
+    )
+
+    response = client.delete(
+        "/ingredients/1",
+        headers=headers,
+        json=MockIngredient.random_valid_ingredient_data(),
+    )
+    assert response.status_code == 401
+    data = json.loads(response.data)
+    assert data == {"code": 401, "message": "Unauthorized."}
+
+
+@pytest.mark.parametrize(
+    "headers",
+    [
+        {},
+        {"Authorization": ""},
+        {"Authorization": "Bearer"},
+    ],
+)
+def test_401_create_recipe_authorization_invalid(
+    headers: dict[str, str],
+    client: FlaskClient,
+) -> None:
     response = client.post(
         "/recipes/",
+        headers=headers,
+        json=MockRecipe.random_valid_recipe_data(),
+    )
+    assert response.status_code == 401
+    data = json.loads(response.data)
+    assert data == {"code": 401, "message": "Unauthorized."}
+
+
+@pytest.mark.parametrize(
+    "headers",
+    [
+        {},
+        {"Authorization": ""},
+        {"Authorization": "Bearer"},
+    ],
+)
+def test_401_delete_recipe_authorization_invalid(
+    headers: dict[str, str],
+    client: FlaskClient,
+) -> None:
+    response = client.post(
+        "/recipes/",
+        headers=MockAuth.authorization_header(auth.CREATE_RECIPE_PERMISSION),
+        json=MockRecipe.random_valid_recipe_data(),
+    )
+
+    response = client.delete(
+        "/recipes/1",
         headers=headers,
         json=MockRecipe.random_valid_recipe_data(),
     )
