78dffcaee34256e9cc3434d15f81dd595d2a695c
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-27 21:05:36 +0900

    Add creating recipe service.

--- frontend/src/interfaces/recipe.ts
+++ frontend/src/interfaces/recipe.ts
@@ -1,12 +1,28 @@
 import { type IngredientBase } from './ingredient';
 
 export interface RecipeBase {
-  id: number;
   name: string;
   description: string | null;
   image_uri: string | null;
 }
 
-export interface Recipe extends RecipeBase {
+export interface RecipeCreate extends RecipeBase {}
+
+export interface RecipeWithID extends RecipeBase {
+  id: number;
+}
+
+export interface Recipe extends RecipeWithID {
   ingredients: Array<IngredientBase>;
+}
+
+export class TRecipe implements RecipeBase {
+  name: string;
+  description: string | null;
+  image_uri: string | null;
+  constructor(name: string, description: string | null, image_uri: string | null) {
+    this.name = name;
+    this.description = description;
+    this.image_uri = image_uri;
+  }
 }
--- frontend/src/services/recipe.ts
+++ frontend/src/services/recipe.ts
@@ -1,8 +1,34 @@
 import { useFetchWithParsable } from '@/composables/fetch';
 import { apiUrl } from '@/env';
+import { TRecipe } from '@/interfaces/recipe';
 import { RecipesResponseSchema } from '@/validator/recipe';
+import { ref } from 'vue';
 
 export function getRecipes() {
   const { data, error } = useFetchWithParsable(RecipesResponseSchema, `${apiUrl}/recipes`);
   return { recipesResponse: data, error };
+}
+
+export function createRecipe() {
+  const recipeCreate = ref<TRecipe>(new TRecipe('', null, null));
+  const error = ref<Error>();
+  const create = async () => {
+    try {
+      error.value = undefined;
+
+      const response = await fetch(`${apiUrl}/recipes`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+          authorization: 'bearer create:recipe'
+        },
+        body: JSON.stringify(recipeCreate.value)
+      });
+      const result = await response.json();
+      console.log(result.message);
+    } catch (e) {
+      error.value = e as Error;
+    }
+  };
+  return { recipeCreate, create };
 }
