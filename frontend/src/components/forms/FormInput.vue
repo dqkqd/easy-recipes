49f7a6351e7b2042a58db0749d34d9f4aff96e4d
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-27 21:05:00 +0900

    Create simple form input.

---
+++ frontend/src/components/forms/FormInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <label>{{ props.label }}:</label>
+    <input
+      :placeholder="props.label"
+      :value="props.modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  label: string;
+  modelValue: string | null;
+}>();
+</script>
+
+<style scoped></style>
