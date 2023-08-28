25e5578a18767e0982e87cf0d8b23a88473f7199
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-28 20:42:22 +0900

    define emit with types.

--- frontend/src/components/forms/FormInput.vue
+++ frontend/src/components/forms/FormInput.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
     <label>{{ props.label }}:</label>
-    <input
-      :placeholder="props.label"
-      :value="props.modelValue"
-      @input="$emit('update:modelValue', $event.target.value)"
-    />
+    <input :placeholder="props.label" v-model="value" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+
 const props = defineProps<{
   label: string;
   modelValue: string | null;
 }>();
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string | null): void;
+}>();
+
+const value = computed({
+  get() {
+    return props.modelValue;
+  },
+
+  set(value) {
+    emit('update:modelValue', value);
+  }
+});
 </script>
 
 <style scoped></style>
