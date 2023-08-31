428c26268b5c017cd161888571013ea7b07b6018
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-31 18:37:28 +0900

    Convert image internally.

--- frontend/src/interfaces/recipe.ts
+++ frontend/src/interfaces/recipe.ts
@@ -1,3 +1,4 @@
+import { convertFileServerDev } from '@/env';
 import { IngredientBase, type IIngredientBase } from './ingredient';
 
 export interface IRecipeBase {
@@ -25,6 +26,22 @@ export class RecipeBase implements IRecipeBase {
     this.description = description;
     this.image_uri = image_uri;
   }
+
+  static from_object(object: {
+    name: string;
+    description: string | null;
+    image_uri: string | null;
+  }) {
+    const { name, description, image_uri } = object;
+    return new RecipeBase(name, description, image_uri);
+  }
+
+  with_converted_image() {
+    if (this.image_uri) {
+      this.image_uri = convertFileServerDev(this.image_uri);
+    }
+    return this;
+  }
 }
 
 export class RecipeCreate extends RecipeBase {}
@@ -43,5 +60,16 @@ export class Recipe extends RecipeBase implements IRecipe {
     super(name, description, image_uri);
     this.id = id;
     this.ingredients = ingredients;
+  }
+
+  static from_object(object: {
+    id: number;
+    name: string;
+    description: string | null;
+    image_uri: string | null;
+    ingredients: IngredientBase[];
+  }) {
+    const { id, name, description, image_uri, ingredients } = object;
+    return new Recipe(id, name, description, image_uri, ingredients);
   }
 }
--- frontend/src/views/recipe/RecipeDetails.vue
+++ frontend/src/views/recipe/RecipeDetails.vue
@@ -3,7 +3,7 @@
   <div v-else-if="result">
     <h1 data-test="recipe-details-name">{{ result.name }}</h1>
     <p data-test="recipe-details-description">{{ result.description }}</p>
-    <img v-if="image_uri" :src="image_uri" data-test="recipe-details-valid-image" />
+    <img v-if="result.image_uri" :src="result.image_uri" data-test="recipe-details-valid-image" />
     <img v-else src="/no-image-icon.png" data-test="recipe-details-default-image" />
   </div>
   <div v-else data-test="recipe-details-loading">Loading ...</div>
@@ -11,15 +11,15 @@
 
 <script setup lang="ts">
 import { useAxios } from '@/composables/fetch';
-import { apiUrl, convertFileServerDev } from '@/env';
-import type { Recipe } from '@/interfaces/recipe';
+import { apiUrl } from '@/env';
+import { Recipe } from '@/interfaces/recipe';
 import { RecipeSchema } from '@/validator/recipe';
-import { computed, onMounted } from 'vue';
+import { onMounted } from 'vue';
 
 const props = defineProps<{ id: number | string }>();
 
 const { result, error, execute } = useAxios<Recipe>((r) => {
-  return RecipeSchema.parse(r.data);
+  return Recipe.from_object(RecipeSchema.parse(r.data)).with_converted_image();
 });
 
 onMounted(async () => {
@@ -27,13 +27,6 @@ onMounted(async () => {
     method: 'get',
     url: `${apiUrl}/recipes/${props.id}`
   });
-});
-
-const image_uri = computed(() => {
-  if (result.value && result.value.image_uri) {
-    return convertFileServerDev(result.value.image_uri);
-  }
-  return null;
 });
 </script>
 
