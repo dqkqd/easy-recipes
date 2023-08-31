f6bdfad9372f8089ad977b830d1c9721f818672f
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-31 21:09:14 +0900

    Convert image.

--- frontend/src/views/recipe/RecipeDetails.vue
+++ frontend/src/views/recipe/RecipeDetails.vue
@@ -3,7 +3,7 @@
   <div v-else-if="result">
     <h1 data-test="recipe-details-name">{{ result.name }}</h1>
     <p data-test="recipe-details-description">{{ result.description }}</p>
-    <img v-if="result.image_uri" :src="result.image_uri" data-test="recipe-details-valid-image" />
+    <img v-if="image_uri" :src="image_uri" data-test="recipe-details-valid-image" />
     <img v-else src="/no-image-icon.png" data-test="recipe-details-default-image" />
   </div>
   <div v-else data-test="recipe-details-loading">Loading ...</div>
@@ -11,14 +11,21 @@
 
 <script setup lang="ts">
 import { useAxios } from '@/composables';
-import { apiUrl } from '@/env';
+import { apiUrl, convertFileServerDev } from '@/env';
 import { RecipeSchema, type Recipe } from '@/schema/recipe';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 
 const props = defineProps<{ id: number | string }>();
 
 const { result, error, execute } = useAxios<Recipe>((r) => {
   return RecipeSchema.parse(r.data);
+});
+
+const image_uri = computed(() => {
+  if (result.value) {
+    return convertFileServerDev(result.value?.image_uri);
+  }
+  return null;
 });
 
 onMounted(async () => {
