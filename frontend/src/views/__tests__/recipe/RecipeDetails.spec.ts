13b794739f5b484d3be2d39ffa4c88db2fde3343
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-01 04:21:29 +0900

    Refactor tests.
    - Move duplicated functions to higher scope.
    - Add render tests for delete button.
    - Remove unncessary rendering tests.

--- frontend/src/views/__tests__/recipe/RecipeDetails.spec.ts
+++ frontend/src/views/__tests__/recipe/RecipeDetails.spec.ts
@@ -8,138 +8,110 @@ import axios from 'axios';
 import { useRouter } from 'vue-router';
 
 describe('RecipeDetails', () => {
-  it('Render recipe successfully', async () => {
-    const recipe = RecipeSchema.parse({
-      id: 1,
-      name: 'Recipe name',
-      description: 'Recipe description',
-      image_uri: 'http://localhost:8080/valid-image-url',
-      ingredients: []
-    });
-
-    vi.spyOn(axios, 'request').mockResolvedValue({
-      data: recipe
-    });
-
-    const wrapper = mount(RecipeDetails, { props: { id: 1 } });
-    await flushPromises();
-
-    expect(axios.request).toBeCalledTimes(1);
-    expect(axios.request).toBeCalledWith({
-      method: 'get',
-      url: `${apiUrl}/recipes/${recipe.id}`
-    });
-
-    expect(wrapper.find('[data-test=recipe-details-name]').text()).toBe(recipe.name);
-    expect(wrapper.find('[data-test=recipe-details-description]').text()).toBe(recipe.description);
-    expect(wrapper.find('[data-test=recipe-details-valid-image]').attributes('src')).toBe(
-      recipe.image_uri
-    );
-
-    expect(wrapper.find('[data-test=recipe-details-error]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-loading]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-default-image]').exists()).toBe(false);
+  const recipe = RecipeSchema.parse({
+    id: 1,
+    name: 'Recipe name',
+    description: 'Recipe description',
+    image_uri: 'http://localhost:8080/valid-image-url',
+    ingredients: []
   });
 
-  it('Render default image successfully', async () => {
-    const recipe = RecipeSchema.parse({
-      id: 1,
-      name: 'Recipe name',
-      description: 'Recipe description',
-      image_uri: null,
-      ingredients: []
+  describe('Render properly', () => {
+    it('Render recipe successfully', async () => {
+      vi.spyOn(axios, 'request').mockResolvedValue({
+        data: recipe
+      });
+
+      const wrapper = mount(RecipeDetails, { props: { id: 1 } });
+      await flushPromises();
+
+      expect(axios.request).toBeCalledTimes(1);
+      expect(axios.request).toBeCalledWith({
+        method: 'get',
+        url: `${apiUrl}/recipes/${recipe.id}`
+      });
+
+      expect(wrapper.find('[data-test=recipe-details-name]').text()).toBe(recipe.name);
+      expect(wrapper.find('[data-test=recipe-details-description]').text()).toBe(
+        recipe.description
+      );
+      expect(wrapper.find('[data-test=recipe-details-valid-image]').attributes('src')).toBe(
+        recipe.image_uri
+      );
+
+      expect(wrapper.find('[data-test=recipe-details-error]').exists()).toBe(false);
+      expect(wrapper.find('[data-test=recipe-details-loading]').exists()).toBe(false);
+      expect(wrapper.find('[data-test=recipe-details-default-image]').exists()).toBe(false);
+      expect(wrapper.find('[data-test=delete-button]').exists()).toBe(true);
     });
-    vi.spyOn(axios, 'request').mockResolvedValue({
-      data: recipe
+
+    it('Render default image successfully', async () => {
+      const recipeWithNullImage = recipe;
+      recipeWithNullImage.image_uri = null;
+
+      vi.spyOn(axios, 'request').mockResolvedValue({
+        data: recipeWithNullImage
+      });
+
+      const wrapper = mount(RecipeDetails, { props: { id: 1 } });
+      await flushPromises();
+
+      expect(axios.request).toBeCalledTimes(1);
+      expect(axios.request).toBeCalledWith({
+        method: 'get',
+        url: `${apiUrl}/recipes/${recipeWithNullImage.id}`
+      });
+
+      expect(wrapper.find('[data-test=recipe-details-name]').text()).toBe(recipeWithNullImage.name);
+      expect(wrapper.find('[data-test=recipe-details-description]').text()).toBe(
+        recipeWithNullImage.description
+      );
+      expect(wrapper.find('[data-test=recipe-details-default-image]').attributes('src')).toBe(
+        '/no-image-icon.png'
+      );
     });
 
-    const wrapper = mount(RecipeDetails, { props: { id: 1 } });
-    await flushPromises();
+    it('Render with loading', async () => {
+      vi.spyOn(axios, 'request').mockResolvedValue({
+        data: recipe
+      });
 
-    expect(axios.request).toBeCalledTimes(1);
-    expect(axios.request).toBeCalledWith({
-      method: 'get',
-      url: `${apiUrl}/recipes/${recipe.id}`
+      const wrapper = mount(RecipeDetails, { props: { id: 1 } });
+
+      expect(wrapper.find('[data-test=recipe-details-loading]').text()).toBe('Loading ...');
     });
 
-    expect(wrapper.find('[data-test=recipe-details-name]').text()).toBe(recipe.name);
-    expect(wrapper.find('[data-test=recipe-details-description]').text()).toBe(recipe.description);
-    expect(wrapper.find('[data-test=recipe-details-default-image]').attributes('src')).toBe(
-      '/no-image-icon.png'
-    );
+    it('Render with error', async () => {
+      vi.spyOn(axios, 'request').mockImplementation(() => {
+        throw new Error('Unexpected error');
+      });
 
-    expect(wrapper.find('[data-test=recipe-details-error]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-loading]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-valid-image]').exists()).toBe(false);
+      const wrapper = mount(RecipeDetails, { props: { id: 1 } });
+      await flushPromises();
+
+      expect(axios.request).toBeCalledTimes(1);
+      expect(axios.request).toBeCalledWith({
+        method: 'get',
+        url: `${apiUrl}/recipes/1`
+      });
+      expect(wrapper.find('[data-test=recipe-details-error]').text()).toBe('Something wrong ...');
+    });
   });
 
-  it('Render with loading', async () => {
-    const recipe = RecipeSchema.parse({
-      id: 1,
-      name: 'Recipe name',
-      description: 'Recipe description',
-      image_uri: null,
-      ingredients: []
-    });
-    vi.spyOn(axios, 'request').mockResolvedValue({
-      data: recipe
-    });
-
-    const wrapper = mount(RecipeDetails, { props: { id: 1 } });
-
-    expect(wrapper.find('[data-test=recipe-details-loading]').text()).toBe('Loading ...');
-
-    expect(wrapper.find('[data-test=recipe-details-error]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-name]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-description]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-valid-image]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-default-image]').exists()).toBe(false);
-  });
-
-  it('Render with error', async () => {
-    vi.spyOn(axios, 'request').mockImplementation(() => {
-      throw new Error('Unexpected error');
-    });
-
-    const wrapper = mount(RecipeDetails, { props: { id: 1 } });
-    await flushPromises();
-
-    expect(axios.request).toBeCalledTimes(1);
-    expect(axios.request).toBeCalledWith({
-      method: 'get',
-      url: `${apiUrl}/recipes/1`
-    });
-    expect(wrapper.find('[data-test=recipe-details-error]').text()).toBe('Something wrong ...');
-
-    expect(wrapper.find('[data-test=recipe-details-name]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-description]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-valid-image]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-default-image]').exists()).toBe(false);
-    expect(wrapper.find('[data-test=recipe-details-loading]').exists()).toBe(false);
-  });
-
-  describe('Test Delete Button', () => {
+  describe('Delete Recipe', () => {
     vi.mock('vue-router', () => ({
       useRouter: vi.fn(() => ({
         push: () => {}
       }))
     }));
 
+    const push = vi.fn();
+    // @ts-ignore
+    useRouter.mockImplementation(() => ({
+      push
+    }));
+
     it('Delete successfully', async () => {
-      const recipe = RecipeSchema.parse({
-        id: 1,
-        name: 'Recipe name',
-        description: 'Recipe description',
-        image_uri: 'http://localhost:8080/valid-image-url',
-        ingredients: []
-      });
-
-      const push = vi.fn();
-      // @ts-ignore
-      useRouter.mockImplementationOnce(() => ({
-        push
-      }));
-
       vi.spyOn(axios, 'request').mockResolvedValueOnce({
         data: recipe
       });
@@ -175,20 +147,6 @@ describe('RecipeDetails', () => {
     });
 
     it('Delete with error', async () => {
-      const recipe = RecipeSchema.parse({
-        id: 1,
-        name: 'Recipe name',
-        description: 'Recipe description',
-        image_uri: 'http://localhost:8080/valid-image-url',
-        ingredients: []
-      });
-
-      const push = vi.fn();
-      // @ts-ignore
-      useRouter.mockImplementationOnce(() => ({
-        push
-      }));
-
       vi.spyOn(axios, 'request').mockResolvedValueOnce({
         data: recipe
       });
