38e09dabb5d6a24a275fd060a8022936e128af9e
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-08-31 20:56:00 +0900

    Remove unused function.

--- frontend/src/composables/fetch.ts
+++ frontend/src/composables/fetch.ts
@@ -1,50 +1,6 @@
 import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 import axios from 'axios';
-import { ref, toValue, watchEffect, type Ref } from 'vue';
-
-export function useFetch(url: string) {
-  const data = ref();
-  const error = ref<Error>();
-
-  watchEffect(async () => {
-    data.value = null;
-    error.value = undefined;
-    const urlValue = toValue(url);
-
-    try {
-      const res = await fetch(urlValue);
-      data.value = await res.json();
-    } catch (e) {
-      error.value = e as Error;
-    }
-  });
-
-  return { data, error };
-}
-
-export function useFetchWithParsable<Type>(
-  c: { parse(data: unknown): Type },
-  url: string
-): { data: Ref<Type>; error: Ref<Error | undefined> } {
-  const data = ref();
-  const error = ref<Error>();
-
-  watchEffect(async () => {
-    data.value = null;
-    error.value = undefined;
-    const urlValue = toValue(url);
-
-    try {
-      const res = await fetch(urlValue);
-      const jsonData = await res.json();
-      data.value = c.parse(jsonData);
-    } catch (e) {
-      error.value = e as Error;
-    }
-  });
-
-  return { data, error };
-}
+import { ref, type Ref } from 'vue';
 
 interface UseAxiosResult<
   T,
--- frontend/src/services/recipe.ts
+++
@@ -1,8 +0,0 @@
-import { useFetchWithParsable } from '@/composables/fetch';
-import { apiUrl } from '@/env';
-import { RecipesResponseSchema } from '@/schema/recipe';
-
-export function getRecipes() {
-  const { data, error } = useFetchWithParsable(RecipesResponseSchema, `${apiUrl}/recipes/`);
-  return { recipesResponse: data, error };
-}
