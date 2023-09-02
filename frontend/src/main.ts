3f9a82ad9ee58dfe67130be86113c04201c80387
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-02 09:19:33 +0900

    Add vuetify.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,11 +6,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import vuetify from '@/components/plugins/vuetify';
 import './assets/main.css';
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(vuetify);
 app.use(router);
 
 app.mount('#app');
