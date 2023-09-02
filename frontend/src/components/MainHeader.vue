fae3a4ce7ea8e344329c84e4e2216e2f0bd48b68
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-02 10:52:41 +0900

    Use vuetify button.

--- frontend/src/components/MainHeader.vue
+++ frontend/src/components/MainHeader.vue
@@ -1,29 +1,20 @@
 <template>
-  <div class="main-header">
-    <router-link :to="{ name: 'home' }" class="app-title text-scale-on-hover"
-      >Easy Recipes</router-link
-    >
-    <nav class="app-content">
-      <router-link :to="{ name: 'RecipeView' }" class="app-content-link text-scale-on-hover"
-        >Recipes</router-link
-      >
-      <router-link :to="{ name: 'IngredientView' }" class="app-content-link text-scale-on-hover"
-        >Ingredients</router-link
-      >
-    </nav>
-  </div>
+  <VLayout class="rounded rounded-md">
+    <VAppBar>
+      <VAppBarTitle class="overflow-x-visible">
+        <VBtn height="100vh" @click="$router.push({ name: 'home' })">
+          <a class="app-title">Easy Recipes</a></VBtn
+        >
+        <VBtn height="100vh" @click="$router.push({ name: 'RecipeView' })" text="Recipes" />
+        <VBtn height="100vh" @click="$router.push({ name: 'IngredientView' })" text="Ingredients" />
+      </VAppBarTitle>
+    </VAppBar>
+  </VLayout>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
-.main-header {
-  display: flex;
-  align-items: center;
-  padding-bottom: 10px;
-  background-color: #dce0e8;
-}
-
 .app-title {
   width: 230px;
   font-size: 30px;
@@ -32,24 +23,5 @@
   text-align: center;
   text-decoration: none;
   color: #4c4f69;
-}
-
-.app-content {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-
-.app-content-link {
-  padding-left: 30px;
-  height: 100%;
-  font-size: 20px;
-  text-decoration: none;
-  padding: 0 10px;
-  color: #4c4f69;
-}
-
-.text-scale-on-hover:hover {
-  transform: scale(1.05);
 }
 </style>
