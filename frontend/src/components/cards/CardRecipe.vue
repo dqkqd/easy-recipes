c4fa23883f5e046584353549fb19f5135d3432a4
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-06 00:54:58 +0900

    Don't need to use compute for recipe's image.

--- frontend/src/components/cards/CardRecipe.vue
+++ frontend/src/components/cards/CardRecipe.vue
@@ -1,14 +1,14 @@
 <template>
   <VHover v-slot="hover">
     <VCard
-      @click="toRecipeDetails"
+      @click="router.push({ name: 'RecipeDetails', params: { id: recipe.id } })"
       :elevation="hover && hover.isHovering ? 12 : 2"
       data-test="card-to-recipe-details"
       v-bind="hover && hover.props"
       :class="{ 'recipe-on-hover': hover && hover.isHovering }"
     >
       <VAvatar class="ma-3" :size="256" :rounded="0">
-        <VImg :src="imageUri" data-test="card-recipe-image"></VImg>
+        <VImg :src="convertFileServerDev(recipe.image_uri)" data-test="card-recipe-image"></VImg>
       </VAvatar>
       <VCardTitle class="text-h5 text-center font-weight-bold" data-test="card-recipe-name">
         {{ recipe.name }}
@@ -20,23 +20,13 @@
 <script setup lang="ts">
 import { convertFileServerDev } from '@/env';
 import type { Recipe } from '@/schema/recipe';
-import { computed } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const props = defineProps<{
+defineProps<{
   recipe: Recipe;
 }>();
-
-const imageUri = computed(() => {
-  const image = convertFileServerDev(props.recipe.image_uri);
-  return image || '/no-image-icon.png';
-});
-
-function toRecipeDetails() {
-  router.push({ name: 'RecipeDetails', params: { id: props.recipe.id } });
-}
 </script>
 
 <style scoped>
--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -2,14 +2,16 @@ const env = import.meta.env;
 let _apiUrl = '';
 let _fileServerURL = '';
 
+const defaultImage = '/no-image-icon.png';
+
 if (env.MODE == 'development') {
   _apiUrl = `http://${env.VITE_API_HOST_DEV}`;
   _fileServerURL = `http://${env.VITE_FILE_SERVER_HOST}`;
 }
 
-export function convertFileServerDev(url: string | null) {
+export function convertFileServerDev(url: string | null | undefined) {
   if (!url) {
-    return url;
+    return defaultImage;
   }
 
   if (env.MODE != 'development') {
