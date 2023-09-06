b7475b3de20447ea359fd924e43d9ad007dcfa1c
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-06 23:13:55 +0900

    Baseline for RecipeDetails.

--- frontend/src/views/recipe/RecipeDetails.vue
+++ frontend/src/views/recipe/RecipeDetails.vue
@@ -1,26 +1,70 @@
 <template>
-  <VApp>
+  <VLayout>
     <MainAppBar />
-    <div v-if="error" data-test="recipe-details-error">Something wrong ...</div>
-    <div v-else-if="result">
-      <h1 data-test="recipe-details-name">{{ result.name }}</h1>
-      <p data-test="recipe-details-description">{{ result.description }}</p>
-      <img v-if="image_uri" :src="image_uri" data-test="recipe-details-valid-image" />
-      <img v-else src="/no-image-icon.png" data-test="recipe-details-default-image" />
-      <div>
-        <DeleteButton label="Delete recipe" @close="deleteRecipe" />
-        <div v-if="deletedError" data-test="recipe-details-delete-error">
-          Error while deleting ...
-        </div>
-        <div v-else-if="isDeleteing" data-test="recipe-details-delete-loading">Deleting ...</div>
-      </div>
-    </div>
-    <div v-else data-test="recipe-details-loading">Loading ...</div>
-  </VApp>
+    <VMain class="d-flex align-center justify-center">
+      <VSheet :width="800" class="my-8 text-center">
+        <VSheet v-if="result" border>
+          <VRow class="my-6 mx-3" justify="center" no-gutters>
+            <VCol>
+              <VImg
+                :height="500"
+                :max-width="500"
+                :src="image_uri"
+                class="align-end"
+                data-test="recipe-details-image"
+                cover
+              >
+              </VImg>
+            </VCol>
+
+            <VCol class="mx-3">
+              <VSheet :height="460">
+                <div class="font-weight-bold text-h4 text-wrap text-center">
+                  {{ result.name }}
+                </div>
+
+                <VDivider />
+
+                <div class="text-h6 ma-3">
+                  {{ result.description }}
+                </div>
+              </VSheet>
+
+              <VRow align="center">
+                <VCol cols="8">
+                  <VHover v-slot="{ isHovering, props }" close-delay="200">
+                    <VIcon
+                      icon="mdi-heart"
+                      v-bind="props"
+                      :color="isHovering ? 'red-darken-1' : 'red-lighten-2'"
+                      :size="40"
+                    />
+                    <span class="mx-2 font-weight-bold">0 people like it</span>
+                  </VHover>
+                </VCol>
+
+                <VCol cols="2">
+                  <VBtn icon="mdi-pencil" />
+                </VCol>
+                <VCol cols="2">
+                  <VBtn icon="mdi-delete" />
+                </VCol>
+              </VRow>
+            </VCol>
+          </VRow>
+        </VSheet>
+
+        <VContainer v-else-if="isLoading">
+          <VProgressCircular :size="50" indeterminate></VProgressCircular>
+        </VContainer>
+
+        <VContainer> //TODO </VContainer>
+      </VSheet>
+    </VMain>
+  </VLayout>
 </template>
 
 <script setup lang="ts">
-import DeleteButton from '@/components/buttons/DeleteButton.vue';
 import MainAppBar from '@/components/navs/MainAppBar.vue';
 import { useAxios } from '@/composables';
 import { apiUrl, convertFileServerDev } from '@/env';
@@ -37,15 +81,13 @@ const router = useRouter();
 
 const props = defineProps<{ id: number | string }>();
 
-const { result, error, execute } = useAxios<Recipe>((r) => {
+const { result, isLoading, error, execute } = useAxios<Recipe>((r) => {
   return RecipeSchema.parse(r.data);
 });
 
+isLoading.value = true;
 const image_uri = computed(() => {
-  if (result.value) {
-    return convertFileServerDev(result.value?.image_uri);
-  }
-  return null;
+  return convertFileServerDev(result.value?.image_uri);
 });
 
 const {
@@ -79,5 +121,11 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped></style>
-@/schema/recipe
+<style scoped>
+.v-icon {
+  transition: opacity 0.4s ease-in-out;
+}
+.v-icon:not(.on-hover) {
+  opacity: 0.8;
+}
+</style>
