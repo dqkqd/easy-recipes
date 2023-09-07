c3babb6eee0adbdedc89043ca7968096c807165e
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-07 20:51:20 +0900

    Implement card warning and add tests.

---
+++ frontend/src/components/__tests__/cards/CardWarning.cy.ts
@@ -0,0 +1,52 @@
+import CardWarning from '@/components/cards/CardWarning.vue';
+import { h } from 'vue';
+
+it('Render properly', () => {
+  const props = {
+    title: 'My card title',
+    acceptLabel: 'My accept label',
+    cancelLabel: 'My cancel label'
+  };
+
+  cy.mount(() => h(CardWarning, props))
+    .get('[data-test=card-warning-title]')
+    .should('have.text', props.title)
+
+    .get('[data-test=card-warning-accept-button]')
+    .should('have.text', props.acceptLabel)
+
+    .get('[data-test=card-warning-cancel-button]')
+    .should('have.text', props.cancelLabel);
+});
+
+it('Render default', () => {
+  cy.mount(() => h(CardWarning))
+    .get('[data-test=card-warning-title]')
+    .should('not.exist')
+
+    .get('[data-test=card-warning-accept-button]')
+    .should('have.text', 'Accept')
+
+    .get('[data-test=card-warning-cancel-button]')
+    .should('have.text', 'Cancel');
+});
+
+it('Emit accept and cancel when clicking', () => {
+  cy.mount(() =>
+    h(CardWarning, {
+      onAccept: cy.spy().as('onAccept'),
+      onCancel: cy.spy().as('onCancel'),
+      onA: cy.spy().as('x')
+    })
+  )
+
+    .get('[data-test=card-warning-accept-button]')
+    .click()
+    .get('@onAccept')
+    .should('have.been.calledOnce')
+
+    .get('[data-test=card-warning-cancel-button]')
+    .click()
+    .get('@onCancel')
+    .should('have.been.calledOnce');
+});
---
+++ frontend/src/components/cards/CardWarning.vue
@@ -0,0 +1,58 @@
+<template>
+  <VSheet border rounded>
+    <div v-if="title" class="text-h5 font-weight-bold ma-4" data-test="card-warning-title">
+      {{ title }}
+    </div>
+    <VDivider />
+    <VRow justify="center" class="ma-2">
+      <VCol cols="5">
+        <VBtn
+          block
+          class="text-h6 text-black font-weight-bold"
+          color="red-accent-2"
+          size="large"
+          variant="flat"
+          @click="$emit('accept')"
+          data-test="card-warning-accept-button"
+        >
+          {{ acceptLabel }}
+        </VBtn>
+      </VCol>
+      <VCol cols="5">
+        <VBtn
+          block
+          class="text-h6 font-weight-bold"
+          color="grey-lighten-3"
+          size="large"
+          variant="flat"
+          @click="$emit('cancel')"
+          data-test="card-warning-cancel-button"
+        >
+          {{ cancelLabel }}
+        </VBtn>
+      </VCol>
+    </VRow>
+  </VSheet>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title?: string;
+    acceptLabel?: string;
+    cancelLabel?: string;
+  }>(),
+  {
+    title: '',
+    acceptLabel: 'Accept',
+    cancelLabel: 'Cancel'
+  }
+);
+
+defineEmits<{
+  (e: 'accept'): void;
+  (e: 'cancel'): void;
+}>();
+</script>
+
+<style scoped></style>
