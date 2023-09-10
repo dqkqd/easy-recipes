c0c4aebcea2edc85cc60418200192954c60eb415
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-10 13:50:07 +0900

    Make tests pass.

--- frontend/src/components/__tests__/cards/CardRecipeCreate.cy.ts
+++ frontend/src/components/__tests__/cards/CardRecipeCreate.cy.ts
@@ -16,83 +16,181 @@ describe('Render', () => {
   });
 });
 
-describe('Add recipe', () => {
-  beforeEach(function () {
+describe('Submit', () => {
+  beforeEach(() => {
     cy.spy(router, 'push')
       .withArgs({ name: 'RecipeInfo', params: { id: 1 } })
       .as('redirectedToRecipeInfo');
+  });
 
-    cy.fixture('recipes/create/1.json').then((validRecipeCreate) => {
+  describe('Success', () => {
+    beforeEach(() => {
+      cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { id: 1 });
+      cy.fixture('recipes/create/1.json')
+        .as('validRecipe')
+        .then((recipe) => {
+          cy.mount(() => h(CardRecipeCreate))
+            .get('[data-test=form-recipe-create-name]')
+            .type(recipe.name)
+            .get('[data-test=form-recipe-create-description]')
+            .type(recipe.description);
+        });
+    });
+
+    afterEach(() => {
+      cy.get('@redirectedToRecipeInfo').should('have.been.called');
+    });
+
+    it('No image selected', function () {
+      const recipe = this.validRecipe;
+      recipe.image_uri = null;
+
       cy.spy(axios, 'request')
         .withArgs({
           method: 'post',
           url: `${apiUrl}/recipes/`,
-          data: validRecipeCreate,
+          data: recipe,
           headers: {
             authorization: 'bearer create:recipe'
           }
         })
         .as('requestToBackEnd');
 
-      cy.mount(() => h(CardRecipeCreate))
-        .get('[data-test=card-recipe-create-form-recipe] [data-test=form-recipe-name] input')
-        .type(validRecipeCreate.name)
-        .get('[data-test=card-recipe-create-form-recipe] [data-test=form-recipe-image-uri] input')
-        .type(validRecipeCreate.image_uri)
-        .get(
-          '[data-test=card-recipe-create-form-recipe] [data-test=form-recipe-description] textarea'
-        )
-        .type(validRecipeCreate.description);
+      cy.get('[data-test=form-recipe-create-submit-button]')
+        .click()
+        .get('@requestToBackEnd')
+        .should('have.been.called');
+    });
+
+    it('Input image url', function () {
+      cy.spy(axios, 'request')
+        .withArgs({
+          method: 'post',
+          url: `${apiUrl}/recipes/`,
+          data: this.validRecipe,
+          headers: {
+            authorization: 'bearer create:recipe'
+          }
+        })
+        .as('requestToBackEnd');
+
+      cy.get('[data-test=form-image-input-url]')
+        .type(this.validRecipe.image_uri)
+        .get('[data-test=form-recipe-create-submit-button]')
+        .click()
+        .get('@requestToBackEnd')
+        .should('have.been.called');
+    });
+
+    it('Input image file', function () {
+      cy.fixture('images/recipe.png', 'base64').then((img) => {
+        const recipe = this.validRecipe;
+        recipe.image_uri = img;
+        cy.spy(axios, 'request')
+          .withArgs({
+            method: 'post',
+            url: `${apiUrl}/recipes/`,
+            data: recipe,
+            headers: {
+              authorization: 'bearer create:recipe'
+            }
+          })
+          .as('requestToBackEnd');
+      });
+
+      cy.get('[data-test=form-image-input-file] input')
+        .selectFile('cypress/fixtures/images/recipe.png')
+        .get('[data-test=form-recipe-create-submit-button]')
+        .click()
+        .get('@requestToBackEnd')
+        .should('have.been.called');
+    });
+
+    it('Loading', () => {
+      cy.get('.v-progress-circular')
+        .should('not.exist')
+        .get('[data-test=form-recipe-create-name] input')
+        .should('not.be.disabled')
+        .get('[data-test=form-recipe-create-description] textarea')
+        .should('not.be.disabled')
+        .get('[data-test=form-image-input-file] input')
+        .should('not.be.disabled')
+        .get('[data-test=form-image-input-url] input')
+        .should('not.be.disabled')
+        .get('[data-test=form-recipe-create-submit-button]')
+        .should('not.be.disabled')
+        .get('[data-test=form-recipe-create-cancel-button]')
+        .should('not.be.disabled')
+
+        .get('[data-test=form-recipe-create-submit-button]')
+        .click()
+
+        .get('.v-progress-circular')
+        .should('be.visible')
+        .get('[data-test=form-recipe-create-name] input')
+        .should('be.disabled')
+        .get('[data-test=form-recipe-create-description] textarea')
+        .should('be.disabled')
+        .get('[data-test=form-image-input-file] input')
+        .should('be.disabled')
+        .get('[data-test=form-image-input-url] input')
+        .should('be.disabled')
+        .get('[data-test=form-recipe-create-cancel-button]')
+        .should('be.disabled');
+
+      cy.once('fail', (err) => {
+        expect(err.message).to.include('`cy.click()` failed because this element');
+        expect(err.message).to.include('`pointer-events: none` prevents user mouse interaction');
+      });
+      cy.get('[data-test=form-recipe-create-submit-button]').click({ timeout: 100 });
     });
   });
 
-  it('Add valid recipe', function () {
-    cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { id: 1 }).as('createRecipe');
+  describe('Failed', () => {
+    beforeEach(() => {
+      cy.spy(axios, 'request').as('requestToBackEnd');
+    });
 
-    cy.get('[data-test=card-recipe-create-form-recipe] [data-test=form-recipe-submit-button]')
-      .click()
+    afterEach(() => {
+      cy.get('@redirectedToRecipeInfo').should('not.have.been.called');
+    });
 
-      .get('@requestToBackEnd')
-      .should('be.called')
+    describe('Invalid response', () => {
+      afterEach(() => {
+        cy.get('@requestToBackEnd').should('have.been.called');
+      });
 
-      .wait('@createRecipe')
+      it('Network error', () => {
+        cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { forceNetworkError: true });
+        cy.mount(() => h(CardRecipeCreate))
+          .get('[data-test=form-recipe-create-name] input')
+          .type('My first recipe')
 
-      .get('@redirectedToRecipeInfo')
-      .should('be.called');
+          .get('[data-test=form-recipe-create-error-dialog]')
+          .should('not.exist')
+          .get('[data-test=form-recipe-create-submit-button]')
+          .click()
+
+          .get('[data-test=form-recipe-create-error-dialog]')
+          .should('be.visible')
+          .wait(2000)
+          .get('[data-test=form-recipe-create-error-dialog]')
+          .should('not.exist');
+      });
+    });
   });
+});
 
-  it('Add recipe with loading', function () {
-    cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { id: 1 }).as('createRecipe');
-
-    cy.get('.v-progress-circular')
-      .should('not.exist')
-
-      .get('[data-test=card-recipe-create-form-recipe] [data-test=form-recipe-submit-button]')
+describe('Cancel', () => {
+  it('Cancel will emit cancel even', () => {
+    cy.mount(() =>
+      h(CardRecipeCreate, {
+        onCancel: cy.spy().as('onCancel')
+      })
+    )
+      .get('[data-test=form-recipe-create-cancel-button]')
       .click()
-
-      .should('exist');
-  });
-
-  it('Add recipe with network error', function () {
-    cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { forceNetworkError: true }).as(
-      'createRecipe'
-    );
-
-    cy.get('[data-test=card-recipe-create-form-recipe] [data-test=form-recipe-submit-button]')
-      .click()
-
-      .get('@requestToBackEnd')
-      .should('be.called')
-
-      .wait('@createRecipe')
-
-      .get('@redirectedToRecipeInfo')
-      .should('not.be.called')
-
-      .get('[data-test=card-recipe-create-error-dialog]')
-      .should('be.visible')
-      .wait(2000)
-      .get('[data-test=card-recipe-create-error-dialog]')
-      .should('not.exist');
+      .get('@onCancel')
+      .should('have.been.called');
   });
 });
--- frontend/src/components/__tests__/forms/FormRecipeCreate.cy.ts
+++ frontend/src/components/__tests__/forms/FormRecipeCreate.cy.ts
@@ -1,12 +1,9 @@
 import FormRecipeCreate from '@/components/forms/FormRecipeCreate.vue';
-import { apiUrl } from '@/env';
-import router from '@/router';
-import axios from 'axios';
 import { h } from 'vue';
 
 describe('Render', () => {
   it('Render properly', () => {
-    cy.mount(() => h(FormRecipeCreate, { loading: false }))
+    cy.mount(() => h(FormRecipeCreate))
 
       .get('[data-test=form-recipe-create-name] label')
       .first()
@@ -24,20 +21,17 @@ describe('Render', () => {
   });
 });
 
-describe.only('Submit', () => {
-  beforeEach(() => {
-    cy.spy(router, 'push')
-      .withArgs({ name: 'RecipeInfo', params: { id: 1 } })
-      .as('redirectedToRecipeInfo');
-  });
-
+describe('Submit', () => {
   describe('Success', () => {
     beforeEach(() => {
-      cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { id: 1 });
       cy.fixture('recipes/create/1.json')
         .as('validRecipe')
         .then((recipe) => {
-          cy.mount(() => h(FormRecipeCreate))
+          cy.mount(() =>
+            h(FormRecipeCreate, {
+              onSubmit: cy.spy().as('onSubmit')
+            })
+          )
             .get('[data-test=form-recipe-create-name]')
             .type(recipe.name)
             .get('[data-test=form-recipe-create-description]')
@@ -45,76 +39,89 @@ describe.only('Submit', () => {
         });
     });
 
-    afterEach(() => {
-      cy.get('@redirectedToRecipeInfo').should('have.been.called');
-    });
-
     it('No image selected', function () {
-      const recipe = this.validRecipe;
-      recipe.image_uri = null;
-
-      cy.spy(axios, 'request')
-        .withArgs({
-          method: 'post',
-          url: `${apiUrl}/recipes/`,
-          data: recipe,
-          headers: {
-            authorization: 'bearer create:recipe'
-          }
-        })
-        .as('requestToBackEnd');
-
       cy.get('[data-test=form-recipe-create-submit-button]')
         .click()
-        .get('@requestToBackEnd')
-        .should('have.been.called');
+        .get('@onSubmit')
+        .should('have.been.calledWith', this.validRecipe.name, null, this.validRecipe.description);
     });
 
     it('Input image url', function () {
-      cy.spy(axios, 'request')
-        .withArgs({
-          method: 'post',
-          url: `${apiUrl}/recipes/`,
-          data: this.validRecipe,
-          headers: {
-            authorization: 'bearer create:recipe'
-          }
-        })
-        .as('requestToBackEnd');
-
       cy.get('[data-test=form-image-input-url]')
         .type(this.validRecipe.image_uri)
         .get('[data-test=form-recipe-create-submit-button]')
         .click()
-        .get('@requestToBackEnd')
-        .should('have.been.called');
+        .get('@onSubmit')
+        .should(
+          'have.been.calledWith',
+          this.validRecipe.name,
+          this.validRecipe.image_uri,
+          this.validRecipe.description
+        );
     });
 
     it('Input image file', function () {
-      cy.fixture('images/recipe.png', 'base64').then((img) => {
-        const recipe = this.validRecipe;
-        recipe.image_uri = img;
-        cy.spy(axios, 'request')
-          .withArgs({
-            method: 'post',
-            url: `${apiUrl}/recipes/`,
-            data: recipe,
-            headers: {
-              authorization: 'bearer create:recipe'
-            }
-          })
-          .as('requestToBackEnd');
-      });
-
+      const file = 'cypress/fixtures/images/recipe.png';
       cy.get('[data-test=form-image-input-file] input')
-        .selectFile('cypress/fixtures/images/recipe.png')
+        .selectFile(file)
         .get('[data-test=form-recipe-create-submit-button]')
         .click()
-        .get('@requestToBackEnd')
-        .should('have.been.called');
+        .get('@onSubmit')
+        .should('have.been.calledOnce');
     });
+  });
 
-    it('Loading', () => {
+  describe('Failed', () => {
+    describe('Invalid form', () => {
+      beforeEach(() => {
+        cy.mount(() =>
+          h(FormRecipeCreate, {
+            onSubmit: cy.spy().as('onSubmit')
+          })
+        )
+          .root()
+          .should('not.contain.text', 'Name is required')
+          .should('not.contain.text', 'Invalid URL');
+      });
+
+      afterEach(() => {
+        cy.get('@onSubmit').should('not.have.been.called');
+      });
+
+      it('Can not create recipe with empty name', () => {
+        cy.get('[data-test=form-recipe-create-submit-button]')
+          .click()
+          .root()
+          .should('contain.text', 'Name is required');
+      });
+
+      it('Can not create recipe with invalid url', function () {
+        cy.get('[data-test=form-image-input-url]')
+          .type('Invalid url')
+          .root()
+          .should('contain.text', 'Invalid URL');
+      });
+    });
+  });
+});
+
+describe('Cancel', () => {
+  it('Cancel will emit cancel even', () => {
+    cy.mount(() =>
+      h(FormRecipeCreate, {
+        onCancel: cy.spy().as('onCancel')
+      })
+    )
+      .get('[data-test=form-recipe-create-cancel-button]')
+      .click()
+      .get('@onCancel')
+      .should('have.been.called');
+  });
+});
+
+describe('Loading', () => {
+  describe('No loading', () => {
+    afterEach(() => {
       cy.get('.v-progress-circular')
         .should('not.exist')
         .get('[data-test=form-recipe-create-name] input')
@@ -128,12 +135,21 @@ describe.only('Submit', () => {
         .get('[data-test=form-recipe-create-submit-button]')
         .should('not.be.disabled')
         .get('[data-test=form-recipe-create-cancel-button]')
-        .should('not.be.disabled')
+        .should('not.be.disabled');
+    });
 
-        .get('[data-test=form-recipe-create-submit-button]')
-        .click()
+    it('default', () => {
+      cy.mount(() => h(FormRecipeCreate));
+    });
 
-        .get('.v-progress-circular')
+    it('Loading=false', () => {
+      cy.mount(() => h(FormRecipeCreate, { loading: false }));
+    });
+  });
+
+  describe('Loading', () => {
+    afterEach(() => {
+      cy.get('.v-progress-circular')
         .should('be.visible')
         .get('[data-test=form-recipe-create-name] input')
         .should('be.disabled')
@@ -152,80 +168,9 @@ describe.only('Submit', () => {
       });
       cy.get('[data-test=form-recipe-create-submit-button]').click({ timeout: 100 });
     });
-  });
 
-  describe('Failed', () => {
-    beforeEach(() => {
-      cy.spy(axios, 'request').as('requestToBackEnd');
+    it('Loading=true', () => {
+      cy.mount(() => h(FormRecipeCreate, { loading: true }));
     });
-
-    afterEach(() => {
-      cy.get('@redirectedToRecipeInfo').should('not.have.been.called');
-    });
-
-    describe('Invalid form', () => {
-      beforeEach(() => {
-        cy.mount(() => h(FormRecipeCreate))
-          .root()
-          .should('not.contain.text', 'Name is required')
-          .should('not.contain.text', 'Invalid URL');
-      });
-
-      afterEach(() => {
-        cy.get('@requestToBackEnd').should('not.have.been.called');
-      });
-
-      it('Can not create recipe with empty name', () => {
-        cy.get('[data-test=form-recipe-create-submit-button]')
-          .click()
-          .root()
-          .should('contain.text', 'Name is required');
-      });
-
-      it('Can not create recipe with invalid url', function () {
-        cy.get('[data-test=form-image-input-url]')
-          .type('Invalid url')
-          .root()
-          .should('contain.text', 'Invalid URL');
-      });
-    });
-
-    describe('Invalid response', () => {
-      afterEach(() => {
-        cy.get('@requestToBackEnd').should('have.been.called');
-      });
-
-      it('Network error', () => {
-        cy.intercept({ method: 'post', url: `${apiUrl}/recipes/` }, { forceNetworkError: true });
-        cy.mount(() => h(FormRecipeCreate))
-          .get('[data-test=form-recipe-create-name] input')
-          .type('My first recipe')
-
-          .get('[data-test=form-recipe-create-error-dialog]')
-          .should('not.exist')
-          .get('[data-test=form-recipe-create-submit-button]')
-          .click()
-
-          .get('[data-test=form-recipe-create-error-dialog]')
-          .should('be.visible')
-          .wait(2000)
-          .get('[data-test=form-recipe-create-error-dialog]')
-          .should('not.exist');
-      });
-    });
-  });
-});
-
-describe('Cancel', () => {
-  it('Cancel will emit cancel even', () => {
-    cy.mount(() =>
-      h(FormRecipeCreate, {
-        onCancel: cy.spy().as('onCancel')
-      })
-    )
-      .get('[data-test=form-recipe-create-cancel-button]')
-      .click()
-      .get('@onCancel')
-      .should('have.been.called');
   });
 });
