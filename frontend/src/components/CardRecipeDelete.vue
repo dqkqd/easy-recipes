cdaca2b77acc9b06d87c221c3f0106cdd8f99768
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-10 23:51:40 +0900

    Add card recipe delete.

---
+++ frontend/src/components/CardRecipeDelete.vue
@@ -0,0 +1,81 @@
+<template>
+  <VDialog
+    persistent
+    width="auto"
+    v-model="isLoading"
+    data-test="card-recipe-delete-loading-dialog"
+  >
+    <VProgressCircular color="red" :size="80" indeterminate />
+  </VDialog>
+
+  <VDialog width="auto" v-model="hasError" data-test="card-recipe-delete-error-dialog">
+    <VAlert prominent :rounded="0" justify="center" type="error" class="px-16 py-5 text-center">
+      <VAlertTitle class="text-h5">Can not delete recipe</VAlertTitle>
+      <div>Please try again later</div>
+    </VAlert>
+  </VDialog>
+
+  <VDialog persistent width="auto" v-model="deleted" data-test="card-recipe-delete-deleted-dialog">
+    <VAlert prominent :rounded="0" justify="center" type="success" class="px-16 py-5 text-center">
+      <VAlertTitle class="text-h5">Recipe deleted</VAlertTitle>
+      <div>You will be redirected shortly...</div>
+    </VAlert>
+  </VDialog>
+
+  <CardWarning
+    title="Are you sure you want to delete your recipe?"
+    accept-label="Delete"
+    cancel-label="Cancel"
+    @accept="deleteRecipe"
+    @cancel="$emit('cancel')"
+    data-test="card-recipe-delete-warning"
+  />
+</template>
+
+<script setup lang="ts">
+import { useAxios } from '@/composables';
+import { apiUrl } from '@/env';
+import { RecipeDeletedResponseSchema, type RecipeDeletedResponse } from '@/schema/recipe';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import CardWarning from './CardWarning.vue';
+
+const props = defineProps<{
+  id: number;
+}>();
+
+const emit = defineEmits<{
+  (e: 'cancel'): void;
+}>();
+const router = useRouter();
+
+const deleted = ref(false);
+const hasError = ref(false);
+
+const { result, error, isLoading, execute } = useAxios<RecipeDeletedResponse>((r) => {
+  return RecipeDeletedResponseSchema.parse(r.data);
+});
+
+async function deleteRecipe() {
+  emit('cancel');
+
+  await execute({
+    method: 'delete',
+    url: `${apiUrl}/recipes/${props.id}`,
+    headers: {
+      authorization: 'bearer delete:recipe'
+    }
+  });
+
+  if (!error.value && result.value) {
+    deleted.value = true;
+    setTimeout(() => {
+      router.push({ name: 'RecipeView' });
+    }, 1500);
+  } else {
+    hasError.value = true;
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/__tests__/CardRecipeDelete.cy.ts
@@ -0,0 +1,102 @@
+import { apiUrl } from '@/env';
+import router from '@/router';
+import axios from 'axios';
+import { h } from 'vue';
+import CardRecipeDelete from '../CardRecipeDelete.vue';
+
+beforeEach(() => {
+  cy.mount(() => h(CardRecipeDelete, { id: 1 }));
+});
+
+describe('Render', () => {
+  it('Render properly', () => {
+    cy.get('[data-test=card-recipe-delete-warning]')
+      .should('be.visible')
+
+      .get('[data-test=card-recipe-delete-loading-dialog]')
+      .should('not.exist')
+
+      .get('[data-test=card-recipe-delete-error-dialog')
+      .should('not.exist')
+
+      .get('[data-test=card-recipe-delete-deleted-dialog]')
+      .should('not.exist');
+  });
+});
+
+describe('Cancel', () => {
+  it('Cancel should emit cancel event', () => {
+    cy.mount(() => h(CardRecipeDelete, { id: 1, onCancel: cy.spy().as('onCancel') }))
+      .get('[data-test=card-warning-cancel-button]')
+      .click()
+      .get('@onCancel')
+      .should('have.been.calledOnce');
+  });
+});
+
+describe('Delete', () => {
+  beforeEach(() => {
+    cy.spy(axios, 'request')
+      .withArgs({
+        method: 'delete',
+        url: `${apiUrl}/recipes/1`,
+        headers: {
+          authorization: 'bearer delete:recipe'
+        }
+      })
+      .as('requestToBackEnd');
+
+    cy.spy(router, 'push').withArgs({ name: 'RecipeView' }).as('redirectedToRecipeVIew');
+  });
+
+  it('Success', () => {
+    cy.intercept({ method: 'delete', url: `${apiUrl}/recipes/1` }, { id: 1 });
+
+    cy.get('[data-test=card-warning-accept-button]')
+      .click()
+
+      .get('@requestToBackEnd')
+      .should('have.been.calledOnce')
+      .get('[data-test=card-recipe-delete-error-dialog]')
+      .should('not.exist')
+      .get('[data-test=card-recipe-delete-deleted-dialog]')
+      .should('be.visible')
+      .should('contain.text', 'Recipe deleted')
+      .should('contain.text', 'You will be redirected shortly...')
+
+      .wait(1500)
+      .get('@redirectedToRecipeVIew')
+      .should('have.been.called');
+  });
+
+  it('Failed', () => {
+    cy.intercept({ method: 'delete', url: `${apiUrl}/recipes/1` }, { forceNetworkError: true });
+
+    cy.get('[data-test=card-warning-accept-button]')
+      .click()
+
+      .get('@requestToBackEnd')
+      .should('have.been.calledOnce')
+
+      .get('[data-test=card-recipe-delete-error-dialog]')
+      .should('be.visible')
+      .should('contain.text', 'Can not delete recipe')
+      .should('contain.text', 'Please try again later')
+
+      .get('@redirectedToRecipeVIew')
+      .should('not.have.been.called');
+  });
+
+  it('Loading', () => {
+    cy.intercept({ method: 'delete', url: `${apiUrl}/recipes/1` }, { delay: 1000 });
+
+    cy.get('[data-test=card-warning-accept-button]')
+      .click()
+
+      .get('@requestToBackEnd')
+      .should('have.been.calledOnce')
+
+      .get('[data-test=card-recipe-delete-loading-dialog]')
+      .should('be.visible');
+  });
+});
