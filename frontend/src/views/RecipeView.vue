0e3af95e16e1ba918526dd00c48abb02be49577b
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-13 07:26:02 +0900

    Use Banner and Pagination components in RecipeView.

--- frontend/src/views/RecipeView.vue
+++ frontend/src/views/RecipeView.vue
@@ -1,81 +1,27 @@
 <template>
   <VApp>
-    <MainAppBar />
-    <VMain>
-      <div v-if="error" data-test="recipe-view-error">Something wrong ...</div>
+    <VLayout>
+      <MainAppBar />
 
-      <div v-else-if="result" data-test="recipe-view-result">
-        <div class="d-flex flex-wrap">
-          <VSheet rounded v-for="recipe in result.recipes" :key="recipe.id" class="pa-2 ma-2">
-            <CardRecipe :recipe="recipe" />
-          </VSheet>
-        </div>
+      <VMain class="bg-grey-lighten-2">
+        <VContainer justify="center">
+          <VRow>
+            <RecipeViewBanner />
+          </VRow>
 
-        <VRow justify="center">
-          <VDialog v-model="dialog" scrollable width="auto">
-            <template v-slot:activator="{ props }">
-              <VBtn v-bind="props">New Recipe</VBtn></template
-            >
-            <CardRecipeCreate />
-          </VDialog>
-        </VRow>
+          <RecipeViewPagination />
+        </VContainer>
 
-        <VPagination
-          :length="pageLength"
-          v-model="currentPage"
-          data-test="recipe-view-pagination"
-        ></VPagination>
-      </div>
-
-      <div v-else data-test="recipe-view-loading">
-        <VProgressLinear indeterminate color="blue"></VProgressLinear>
-      </div>
-    </VMain>
+        <VFooter class="mt-16 pt-16" />
+      </VMain>
+    </VLayout>
   </VApp>
 </template>
 
 <script setup lang="ts">
-import CardRecipe from '@/components/CardRecipe.vue';
-import CardRecipeCreate from '@/components/CardRecipeCreate.vue';
 import MainAppBar from '@/components/MainAppBar.vue';
-import { useAxios } from '@/composables';
-import { apiUrl } from '@/env';
-import { RecipesResponseSchema, type RecipesResponse } from '@/schema/recipe';
-import { computed, onMounted, ref, watch } from 'vue';
-
-const currentPage = ref(1);
-
-const dialog = ref(false);
-
-const { result, error, execute } = useAxios<RecipesResponse>((r) => {
-  return RecipesResponseSchema.parse(r.data);
-});
-
-const pageLength = computed(() => {
-  if (!result.value || result.value.per_page === 0) {
-    return 1;
-  }
-  return Math.ceil(result.value.total / result.value.per_page);
-});
-
-onMounted(async () => {
-  await execute({
-    method: 'get',
-    url: `${apiUrl}/recipes/`
-  });
-});
-
-watch(currentPage, async () => {
-  await execute({
-    method: 'get',
-    url: `${apiUrl}/recipes/?page=${currentPage.value}`
-  });
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-});
+import RecipeViewBanner from '@/components/RecipeViewBanner.vue';
+import RecipeViewPagination from '@/components/RecipeViewPagination.vue';
 </script>
 
 <style scoped></style>
