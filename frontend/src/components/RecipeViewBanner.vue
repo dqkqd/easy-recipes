33660702ed8ee3321fcfad15864f0aa0f840b685
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-13 21:58:41 +0900

    Use BaseViewBanner inside RecipeViewBanner.

--- frontend/src/components/RecipeViewBanner.vue
+++ frontend/src/components/RecipeViewBanner.vue
@@ -1,46 +1,19 @@
 <template>
-  <VImg
-    src="/recipe-cover.jpg"
-    :max-height="220"
-    gradient="to left, rgba(0,0,0,.1), rgba(0,0,0,.7)"
-    cover
-    data-test="recipe-view-banner-cover-image"
+  <BaseViewBanner
+    banner-image="/recipe-cover.jpg"
+    banner-title="Ready to explore more recipes?"
+    button-label="New Recipe"
+    data-test="recipe-view-banner"
   >
-    <VRow no-gutters class="py-10" justify="center" align-content="center" align="center">
-      <VCol cols="9" align-self="center">
-        <div
-          class="text-h2 mx-16 font-weight-black text-white me-auto"
-          data-test="recipe-view-banner-title"
-        >
-          Ready to explore more recipes?
-        </div>
-      </VCol>
-      <VCol cols="3" align-self="center">
-        <VDialog v-model="dialog" scrollable width="auto">
-          <template v-slot:activator="{ props }">
-            <VBtn
-              color="black"
-              v-bind="props"
-              text="New Recipe"
-              class="text-h5 font-weight-black"
-              size="x-large"
-              data-test="recipe-view-banner-new-recipe-button"
-            />
-          </template>
-          <CardRecipeCreate
-            data-test="recipe-view-banner-card-recipe-create"
-            @cancel="dialog = false"
-          />
-        </VDialog>
-      </VCol>
-    </VRow>
-  </VImg>
+    <template #button>
+      <CardRecipeCreate data-test="recipe-view-banner-card-recipe-create" />
+    </template>
+  </BaseViewBanner>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import BaseViewBanner from './BaseViewBanner.vue';
 import CardRecipeCreate from './CardRecipeCreate.vue';
-const dialog = ref(false);
 </script>
 
 <style scoped></style>
--- frontend/src/components/__tests__/RecipeViewBanner.cy.ts
+++ frontend/src/components/__tests__/RecipeViewBanner.cy.ts
@@ -6,26 +6,21 @@ beforeEach(() => {
 });
 
 it('Render', () => {
-  cy.get('[data-test=recipe-view-banner-cover-image] img')
+  cy.get('[data-test=recipe-view-banner] img')
     .should('have.attr', 'src', '/recipe-cover.jpg')
 
-    .get('[data-test=recipe-view-banner-title]')
+    .get('[data-test=recipe-view-banner] [data-test=base-view-banner-title]')
     .should('contain.text', 'Ready to explore more recipes?')
 
-    .get('[data-test=recipe-view-banner-new-recipe-button]')
+    .get('[data-test=recipe-view-banner] [data-test=base-view-banner-button]')
     .should('be.visible');
 });
 
 it('New recipe button', () => {
   cy.get('[data-test=recipe-view-banner-card-recipe-create]')
     .should('not.exist')
-    .get('[data-test=recipe-view-banner-new-recipe-button]')
+    .get('[data-test=recipe-view-banner] [data-test=base-view-banner-button]')
     .click()
     .get('[data-test=recipe-view-banner-card-recipe-create]')
-    .should('be.visible')
-
-    .get('[data-test=form-recipe-cancel-button]')
-    .click()
-    .get('[data-test=recipe-view-banner-card-recipe-create]')
-    .should('not.exist');
+    .should('be.visible');
 });
