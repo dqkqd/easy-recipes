c7c5a6d08eb8d8e36f410a544d200d7e0c3484a1
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-16 19:20:37 +0900

    Apply auth0 into front-end.

--- frontend/src/components/MainAppBar.vue
+++ frontend/src/components/MainAppBar.vue
@@ -23,12 +23,44 @@
         data-test="main-app-bar-ingredients-button"
       />
     </VAppBarTitle>
+    <VBtn
+      v-if="auth.isAuthenticated.value"
+      color="black"
+      @click="logout"
+      text="LOG OUT"
+      class="text-h5 font-weight-black ma-2 mx-16"
+      size="x-large"
+      data-test="base-view-banner-button"
+      variant="outlined"
+      density="comfortable"
+    />
+    <VBtn
+      v-else
+      color="black"
+      @click="login"
+      text="LOG IN"
+      class="text-h5 font-weight-black ma-2 mx-16"
+      size="x-large"
+      data-test="base-view-banner-button"
+      variant="elevated"
+      density="comfortable"
+    />
   </VAppBar>
 </template>
 
 <script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
 import { useRouter } from 'vue-router';
 const router = useRouter();
+const auth = useAuth0();
+
+function login() {
+  auth.loginWithRedirect();
+}
+
+function logout() {
+  auth.logout({ logoutParams: { returnTo: window.location.origin } });
+}
 </script>
 
 <style scoped>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,11 +5,24 @@ import App from './App.vue';
 import router from './router';
 
 import vuetify from '@/plugins/vuetify';
+import { createAuth0 } from '@auth0/auth0-vue';
 import './assets/main.css';
 
 const app = createApp(App);
 
+const env = import.meta.env;
+
 app.use(createPinia());
+app.use(
+  createAuth0({
+    domain: env.VITE_AUTH0_DOMAIN,
+    clientId: env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      audience: env.VITE_AUTH0_API_AUDIENCE
+    }
+  })
+);
 app.use(vuetify);
 app.use(router);
 
