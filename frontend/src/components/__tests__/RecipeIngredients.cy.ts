e299eb53788c1f1d079f1f01ea623bbe15611e6c
Khanh Duong Quoc <dqkqdlot@gmail.com>
2023-09-20 21:27:47 +0900

    Add select tests to RecipeIngredients.

--- frontend/src/components/__tests__/RecipeIngredients.cy.ts
+++ frontend/src/components/__tests__/RecipeIngredients.cy.ts
@@ -1,5 +1,7 @@
 import RecipeIngredients from '@/components/RecipeIngredients.vue';
+import { apiUrl } from '@/env';
 import { RECIPE_INGREDIENT_PER_PAGE } from '@/utils';
+import axios from 'axios';
 import { h } from 'vue';
 
 describe('Render', () => {
@@ -98,5 +100,212 @@ describe('Pagination', () => {
   });
 });
 
-it('Updated ingredients should be changed in pagination pages');
-it('Updated ingredients should be changed in select dialog');
+describe('Updating ingredients', () => {
+  beforeEach(() => {
+    // page 1
+    cy.fixture('ingredients/pages/1.json')
+      .as('firstPageIngredients')
+      .then((ingredients) => {
+        cy.intercept(
+          {
+            method: 'get',
+            url: `${apiUrl}/ingredients/*`,
+            query: {
+              per_page: `${RECIPE_INGREDIENT_PER_PAGE}`
+            }
+          },
+          ingredients
+        );
+      });
+
+    // page 2
+    cy.fixture('ingredients/pages/2.json')
+      .as('secondPageIngredients')
+      .then((ingredients) => {
+        cy.intercept(
+          {
+            method: 'get',
+            url: `${apiUrl}/ingredients/*`,
+            query: {
+              page: '2',
+              per_page: `${RECIPE_INGREDIENT_PER_PAGE}`
+            }
+          },
+          ingredients
+        );
+      });
+
+    cy.fixture('recipes/details/1.json').as('recipe');
+  });
+
+  it('Updated ingredients should be changed in pagination pages', function () {
+    const updatedIngredients = [...this.secondPageIngredients.ingredients.slice(0, 4)].map((v) => {
+      delete v.recipes;
+      return v;
+    });
+
+    cy.signJWT(true, ['update:recipe']).then((token) => {
+      cy.mount(() => h(RecipeIngredients, { recipe: this.recipe }));
+      cy.intercept(
+        { method: 'patch', url: `${apiUrl}/recipes/${this.recipe.id}/ingredients/` },
+        { ...this.recipe, ingredients: updatedIngredients }
+      );
+      cy.spy(axios, 'request')
+        .withArgs({
+          method: 'patch',
+          url: `${apiUrl}/recipes/${this.recipe.id}/ingredients/`,
+          data: {
+            ingredients: [6, 7, 8, 9]
+          },
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        })
+        .as('updateRecipeIngredients');
+    });
+
+    cy.getTestSelector('recipe-ingredients-dialog-button').click();
+    for (const id of [1, 2, 3, 4, 5]) {
+      cy.getTestSelector(`recipe-ingredients-dialog-select-card-${id}`).click();
+    }
+
+    cy.getTestSelector('recipe-ingredients-dialog-select-pagination-pagination')
+      .find('li button span')
+      .each(($elem) => {
+        if ($elem.text() === '2') {
+          cy.wrap($elem).wait(1000).click();
+        }
+      });
+
+    cy.getTestSelector(`recipe-ingredients-dialog-select-card-10`)
+      .click()
+      .getTestSelector('recipe-ingredients-dialog-select-add-button')
+      .click()
+      .get('@updateRecipeIngredients')
+      .should('have.been.called');
+
+    for (const id of [1, 2, 3, 4, 5]) {
+      cy.getTestSelector(`recipe-ingredients-pagination-${id}`).should('not.exist');
+    }
+    for (const id of [6, 7, 8, 9]) {
+      cy.getTestSelector(`recipe-ingredients-pagination-${id}`).should('be.visible');
+    }
+  });
+
+  it('Updated ingredients should be changed in select dialog', function () {
+    const updatedIngredients = [...this.firstPageIngredients.ingredients.slice(0, 3)].map((v) => {
+      delete v.recipes;
+      return v;
+    });
+
+    cy.signJWT(true, ['update:recipe']).then((token) => {
+      cy.mount(() => h(RecipeIngredients, { recipe: this.recipe }));
+      cy.intercept(
+        { method: 'patch', url: `${apiUrl}/recipes/${this.recipe.id}/ingredients/` },
+        { ...this.recipe, ingredients: updatedIngredients }
+      );
+      cy.spy(axios, 'request')
+        .withArgs({
+          method: 'patch',
+          url: `${apiUrl}/recipes/${this.recipe.id}/ingredients/`,
+          data: {
+            ingredients: [1, 2, 3]
+          },
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        })
+        .as('updateRecipeIngredients');
+    });
+
+    cy.getTestSelector('recipe-ingredients-dialog-button').click();
+
+    for (const id of [4, 5]) {
+      cy.getTestSelector(`recipe-ingredients-dialog-select-card-${id}`).click();
+    }
+
+    cy.getTestSelector('recipe-ingredients-dialog-select-pagination-pagination')
+      .find('li button span')
+      .each(($elem) => {
+        if ($elem.text() === '2') {
+          cy.wrap($elem).wait(1000).click();
+        }
+      });
+
+    for (const id of [6, 7, 8, 9, 10]) {
+      cy.getTestSelector(`recipe-ingredients-dialog-select-card-${id}`).click();
+    }
+
+    cy.getTestSelector('recipe-ingredients-dialog-select-add-button')
+      .click()
+      .getTestSelector('recipe-ingredients-dialog-button')
+      .click();
+
+    for (const id of [4, 5]) {
+      cy.getTestSelector(`recipe-ingredients-dialog-select-card-${id}`)
+        .findTestSelector('recipe-ingredients-dialog-select-card')
+        .should('not.have.class', 'selected');
+    }
+
+    for (const id of [1, 2, 3]) {
+      cy.getTestSelector(`recipe-ingredients-dialog-select-card-${id}`)
+        .findTestSelector('recipe-ingredients-dialog-select-card')
+        .should('have.class', 'selected');
+    }
+  });
+
+  it('Update ingredients should change pagination page to page one', function () {
+    const updatedIngredients = [
+      ...this.firstPageIngredients.ingredients,
+      ...this.secondPageIngredients.ingredients.slice(0, 4)
+    ].map((v) => {
+      delete v.recipes;
+      return v;
+    });
+
+    cy.signJWT(true, ['update:recipe']).then((token) => {
+      cy.mount(() => h(RecipeIngredients, { recipe: this.recipe }));
+      cy.intercept(
+        { method: 'patch', url: `${apiUrl}/recipes/${this.recipe.id}/ingredients/` },
+        { ...this.recipe, ingredients: updatedIngredients }
+      );
+      cy.spy(axios, 'request')
+        .withArgs({
+          method: 'patch',
+          url: `${apiUrl}/recipes/${this.recipe.id}/ingredients/`,
+          data: {
+            ingredients: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+          },
+          headers: {
+            authorization: `Bearer ${token}`
+          }
+        })
+        .as('updateRecipeIngredients');
+    });
+
+    cy.getTestSelector('recipe-ingredients-pagination-pagination')
+      .find('button[aria-label="Next page"]')
+      .click()
+      .wait(1000)
+
+      .getTestSelector('recipe-ingredients-dialog-button')
+      .click();
+
+    cy.getTestSelector('recipe-ingredients-dialog-select-pagination-pagination')
+      .find('li button span')
+      .each(($elem) => {
+        if ($elem.text() === '2') {
+          cy.wrap($elem).wait(1000).click();
+        }
+      });
+
+    cy.getTestSelector('recipe-ingredients-dialog-select-card-10')
+      .click()
+      .getTestSelector('recipe-ingredients-dialog-select-add-button')
+      .click();
+
+    for (const id of [1, 2, 3, 4, 5]) {
+      cy.getTestSelector(`recipe-ingredients-pagination-${id}`).should('be.exist');
+    }
+  });
+});
